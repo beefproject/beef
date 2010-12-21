3e47fe214b733eb3ad58e7d8b7b2be6ac6794bc3
mosse.benjamin <mosse.benjamin@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2010-12-21 11:20:06 +0000

    saving my work on the tree panel: list HBs by domain or external ip
    git-svn-id: https://beef.googlecode.com/svn/trunk@628 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

--- public/javascript/ui/panel/ZombieTabs.js
+++ public/javascript/ui/panel/ZombieTabs.js
@@ -8,6 +8,49 @@ ZombieTabs = function(zombie_tree_list) {
 		this.tree_items.push(zombie_tree_list[tree]);
 	}
 	
+	/*
+	 * Update each tree with a new configuration and regenerates them.
+	 * @param: {Literal Object} updated configuration for the trees
+	 */
+	function update_trees_configuration(configuration) {
+		var tree_panel = Ext.getCmp("zombie-tree-tabs-panel");
+		var trees = tree_panel.items;
+		
+		Ext.each(trees.items, function(tree) {
+			tree.updateConfiguration(configuration);
+			tree.reload();
+		});
+	};
+	
+	//the bottom bar for that panel
+	this.bottom_bar = new Ext.Toolbar({
+		items: [
+			{
+				xtype: 'tbtext',
+				text: 'View by:'
+			},
+			{
+				//list the hooked browsers by domain
+				text: 'domain',
+				listeners: {
+					click: function(b) {
+						update_trees_configuration({'sub-branch' : 'domain'});
+					}
+				}
+			},
+			'-',
+			{
+				//list the hooked browsers by external ip
+				text: 'external ip',
+				listeners: {
+					click: function() {
+						alert('under construction');
+					}
+				}
+			}
+		]
+	});
+	
 	MainPanel.superclass.constructor.call(this, {
         id: 'zombie-tree-tabs-panel',
 		title: 'Hooked Browsers',
@@ -20,7 +63,8 @@ ZombieTabs = function(zombie_tree_list) {
         minSize: 175,
         maxSize: 400,
 		deferredRender: false,
-		items: this.tree_items
+		items: this.tree_items,
+		bbar: this.bottom_bar
     });
 };
 
