43e4e261f73a3b5777460e1cb7db2746eecd1990
mosse.benjamin <mosse.benjamin@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2010-12-21 10:04:17 +0000

    saving my work on the tree panel
    git-svn-id: https://beef.googlecode.com/svn/trunk@626 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

--- public/javascript/ui/panel/ZombiesMgr.js
+++ public/javascript/ui/panel/ZombiesMgr.js
@@ -27,37 +27,36 @@ var ZombiesMgr = function(zombies_tree_lists) {
 	 * @param: {Literal Object} an object containing the list of rules from the distributed engine.
 	 */
 	this.updateZombies = function(zombies, rules){
-		var offline_zombies = zombies["offline"];
-		var online_zombies = zombies["online"];
+		var offline_hooked_browsers = zombies["offline"];
+		var online_hooked_browsers = zombies["online"];
 		
 		for(tree_type in this.zombies_tree_lists) {
 			hooked_browsers_tree = this.zombies_tree_lists[tree_type];
 			
 			//we compare and remove the hooked browsers from online and offline branches for each tree.
-			hooked_browsers_tree.compareAndRemove(offline_zombies, false);
-			hooked_browsers_tree.compareAndRemove(online_zombies, true);
+			hooked_browsers_tree.compareAndRemove(zombies);
 			
 			//add an offline browser to the tree
-			for(var i in offline_zombies) {
-				var offline_hooked_browser = this.zombieFactory(i, offline_zombies);
+			for(var i in offline_hooked_browsers) {
+				var offline_hooked_browser = this.zombieFactory(i, offline_hooked_browsers);
 				hooked_browsers_tree.addZombie(offline_hooked_browser, false);
 			}
 			
 			//add an online browser to the tree
-			for(var i in online_zombies) {
-				var online_hooked_browser = this.zombieFactory(i, online_zombies);
+			for(var i in online_hooked_browsers) {
+				var online_hooked_browser = this.zombieFactory(i, online_hooked_browsers);
 				hooked_browsers_tree.addZombie(online_hooked_browser, true);
 				//TODO: add the rules here
 			}
 			
 			//expand the online hooked browser tree lists
-			if(hooked_browsers_tree.online_zombies.childNodes.length > 0) {
-				hooked_browsers_tree.online_zombies.expand(true);
+			if(hooked_browsers_tree.online_hooked_browsers_treenode.childNodes.length > 0) {
+				hooked_browsers_tree.online_hooked_browsers_treenode.expand(true);
 			}
 			
 			//expand the offline hooked browser tree lists
-			if(hooked_browsers_tree.offline_zombies.childNodes.length > 0) {
-				hooked_browsers_tree.offline_zombies.expand(true);
+			if(hooked_browsers_tree.offline_hooked_browsers_treenode.childNodes.length > 0) {
+				hooked_browsers_tree.offline_hooked_browsers_treenode.expand(true);
 			}
 		}
 	}
