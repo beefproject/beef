c52857ac1494a6033566eb62e13c8dfe2fd61687
xntrik <xntrik@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-02-26 04:19:06 +0000

    issue 258 : updated to provide appropriate attribution of the images and more links into the advanced demo
    git-svn-id: https://beef.googlecode.com/svn/trunk@765 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

--- public/javascript/ui/panel/WelcomeTab.js
+++ public/javascript/ui/panel/WelcomeTab.js
@@ -4,7 +4,7 @@ WelcomeTab = function() {
               <div style='font:13px tahoma,arial,helvetica,sans-serif'> \
               <p><img src='/ui/public/images/icons/beef.gif' />&nbsp; <span style='font:bold 15px tahoma,arial,helvetica,sans-serif'>BeEF - The Browser Exploitation Framework</span></p><br /> \
               <p>Welcome to BeEF!</p><br /> \
-              <p>Before being able to fully explore the framework you will have to 'hook' a browser. To begin with you can point a browser towards the demo page <a href='/demos/basic.html' target='_blank'>here</a>.</p><br /> \
+              <p>Before being able to fully explore the framework you will have to 'hook' a browser. To begin with you can point a browser towards the basic demo page <a href='/demos/basic.html' target='_blank'>here</a>, or the advanced version <a href='/demos/butcher/index.html' target='_blank'>here</a>.</p><br /> \
               <p>After a browser is hooked into the framework they will appear in the 'Hooked Browsers' panel on the left. Hooked browsers will appear in either an online or offline state, depending on how recently they have polled the framework. To interact with a hooked browser simply click it, a new tab will appear.</p><br /> \
               <p>Each hooked browser tab has a number of sub-tabs, described below:</p> \
               <p><ul><li><span style='font:bold 13px tahoma,arial,helvetica,sans-serif'>Main:</span> Display information about the hooked browser after you've run some command modules</li> \
