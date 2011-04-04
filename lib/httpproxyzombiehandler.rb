55dfc60b5948904a249065e48ed54d801b32e872
antisnatchor <antisnatchor@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-04-04 14:41:30 +0000

    issue 9: added some server-side logging
    git-svn-id: https://beef.googlecode.com/svn/trunk@850 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

--- lib/httpproxyzombiehandler.rb
+++ lib/httpproxyzombiehandler.rb
@@ -12,7 +12,7 @@ module BeEF
     # the browser will perform the request. Then the results
     # will be sent back to use
     def forward_request(zombie_id, req, res)
-      
+
       # Generate a id for the req in the http table and check it doesnt already exist
       http_id = rand(10000)
       http_db = H.first(:id => http_id) || nil
@@ -21,7 +21,10 @@ module BeEF
         http_id = rand(10000)
         http_db = H.first(:id => http_id) || nil
       end
-            
+
+      # some debug info
+      puts "[PROXY] Forwarding request #" + http_id.to_s + " from zombie [" + zombie_id.to_s + "]" + " to host [" + req.host.to_s + "]"
+
       # Saves the new HTTP request to the db for processing by HB
       http = H.new(
         :id => http_id,
