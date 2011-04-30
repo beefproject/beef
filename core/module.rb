b03d6b52e7b869b68629c0f33d84dbae92818670
passbe <passbe@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-04-30 06:39:06 +0000

    Fixes get_key_by_database_id
    git-svn-id: https://beef.googlecode.com/svn/trunk@945 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

--- core/module.rb
+++ core/module.rb
@@ -30,10 +30,10 @@ module Module
         return false
     end
     
-    # Return module configuration by database id
+    # Return module key by database id
     def self.get_key_by_database_id(id)
         ret = BeEF::Core::Configuration.instance.get('beef.module').select {|k, v| v.has_key?('db') and v['db']['id'].to_i == id.to_i }
-        return (ret.kind_of?(Array)) ? ret.first : ret.keys[0]
+        return (ret.kind_of?(Array)) ? ret.first.first : ret.keys.first
     end
 
     # Returns category name in a system folder format
