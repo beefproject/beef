47876e2c904bc46b56338d32e84115a16444c204
passbe <passbe@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-05-12 23:47:49 +0000

    Stopped storing data type in result table. This will remove the code execution vuln with the renderers engine
    git-svn-id: https://beef.googlecode.com/svn/trunk@970 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

--- core/main/handlers/commands.rb
+++ core/main/handlers/commands.rb
@@ -44,7 +44,7 @@ module Handlers
       command_results = get_param(@data, 'results')
       raise WEBrick::HTTPStatus::BadRequest, "command results empty" if command_results.empty?
       # save the command module results to the datastore and create a log entry
-      command_results = {'type' => command_results.class, 'data' => command_results}
+      command_results = {'data' => command_results}
       BeEF::Core::Models::Command.save_result(beefhook, command_id, command_friendly_name, command_results) 
 
     end
