720864f0ea6e8982a5db3f1697558138e3c60158
passbe <passbe@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-06-26 00:14:31 +0000

    Added new BeEF branding to the admin_ui extension
    git-svn-id: https://beef.googlecode.com/svn/trunk@1021 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

--- extensions/admin_ui/media/javascript/ui/panel/WelcomeTab.js
+++ extensions/admin_ui/media/javascript/ui/panel/WelcomeTab.js
@@ -2,7 +2,7 @@ WelcomeTab = function() {
 
     welcome = " \
               <div style='font:13px tahoma,arial,helvetica,sans-serif'> \
-              <p><img src='/ui/media/images/icons/beef.gif' />&nbsp; <span style='font:bold 15px tahoma,arial,helvetica,sans-serif'>BeEF - The Browser Exploitation Framework</span></p><br /> \
+              <p><img src='/ui/media/images/beef.jpg' alt='BeEF - The Browser Exploitation Framework' /></p><br /> \
               <p>Welcome to BeEF!</p><br /> \
               <p>Before being able to fully explore the framework you will have to 'hook' a browser. To begin with you can point a browser towards the basic demo page <a href='/demos/basic.html' target='_blank'>here</a>, or the advanced version <a href='/demos/butcher/index.html' target='_blank'>here</a>.</p><br /> \
               <p>After a browser is hooked into the framework they will appear in the 'Hooked Browsers' panel on the left. Hooked browsers will appear in either an online or offline state, depending on how recently they have polled the framework. To interact with a hooked browser simply click it, a new tab will appear.</p><br /> \
