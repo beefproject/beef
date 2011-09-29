87020e89f3d512dd07b4a525e2ae5bdc2cc2bc84
bcoles@gmail.com <bcoles@gmail.com@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-09-29 15:25:18 +0000

    Added browser fingerprints
    git-svn-id: https://beef.googlecode.com/svn/trunk@1320 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

--- modules/browser/browser_fingerprinting/command.js
+++ modules/browser/browser_fingerprinting/command.js
@@ -34,12 +34,15 @@ beef.execute(function() {
 
 	// Browser fingerprints // in the form of: "URI","Browser","version(s)"
 	var fingerprints = new Array(
-		new Array("Firefox","moz-icon://.autoreg?size=16"),
-		new Array("Firefox","2.x","resource:///res/html/gopher-audio.gif"),
-		new Array("Firefox","2.x-3.x","jar:resource:///chrome/classic.jar!/skin/classic/browser/Secure.png"),
-		new Array("Firefox","4.x-5.x","resource:///chrome/browser/skin/classic/browser/Secure.png"),
+		new Array("Safari","1+","feed://__rsrc__/__rsrc__/NextPage.tif"),
+		new Array("Firefox","1+","moz-icon://.autoreg?size=16"),
+		new Array("Firefox","2","resource:///res/html/gopher-audio.gif"),
+		new Array("Firefox","2-3","jar:resource:///chrome/classic.jar!/skin/classic/browser/Secure.png"),
+		new Array("Firefox","4-5","resource:///chrome/browser/skin/classic/browser/Secure.png"),
+		new Array("Firefox","1-6","resource:///chrome/browser/content/branding/icon128.png"),
 		new Array("Firefox","4+","resource:///chrome/browser/skin/classic/browser/Geolocation-16.png"),
-		new Array("Internet Explorer","5.x-6.x","res://shdoclc.dll/pagerror.gif"),
+		new Array("Firefox","7+","resource:///chrome/browser/content/browser/aboutHome-snippet1.png"),
+		new Array("Internet Explorer","5-6","res://shdoclc.dll/pagerror.gif"),
 		new Array("Internet Explorer","7+","res://ieframe.dll/info_48.png")
 	);
 
