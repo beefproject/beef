0778a3c32935bbcd9cacce965031b896a9162b18
wade@bindshell.net <wade@bindshell.net@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-09-30 11:32:06 +0000

    basic api test added
    git-svn-id: https://beef.googlecode.com/svn/trunk@1326 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

--- test/unit/ts_beef.rb
+++ test/unit/ts_beef.rb
@@ -11,6 +11,7 @@ require './core/filter/tc_base'
 require './core/filter/tc_command'
 require './core/tc_loader'
 require './core/tc_core'
+require './core/tc_api'
 
 class TS_BeefTests
   def self.suite
@@ -18,6 +19,7 @@ class TS_BeefTests
     suite << TC_Filter.suite
     suite << TC_Loader.suite
     suite << TC_Core.suite
+    suite << TC_Api.suite
     return suite
   end
 end
