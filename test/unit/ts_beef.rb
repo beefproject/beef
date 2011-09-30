ecd7f34986e708d611635031a71dd7e028ff3a78
wade@bindshell.net <wade@bindshell.net@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-09-30 11:31:49 +0000

    basic core test added
    git-svn-id: https://beef.googlecode.com/svn/trunk@1325 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

--- test/unit/ts_beef.rb
+++ test/unit/ts_beef.rb
@@ -10,12 +10,14 @@ end
 require './core/filter/tc_base'
 require './core/filter/tc_command'
 require './core/tc_loader'
+require './core/tc_core'
 
 class TS_BeefTests
   def self.suite
     suite = Test::Unit::TestSuite.new(name="BeEF TestSuite")
     suite << TC_Filter.suite
     suite << TC_Loader.suite
+    suite << TC_Core.suite
     return suite
   end
 end
