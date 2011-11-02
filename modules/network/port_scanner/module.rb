405bdc741cce1be049c28e15bf36b85e6294b581
javier.marcos@gmail.com <javier.marcos@gmail.com@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-11-02 18:21:32 +0000

    Port scanner based on HTTP, CORS or WebSockets. Issue 435: http://code.google.com/p/beef/issues/detail?id=435
    git-svn-id: https://beef.googlecode.com/svn/trunk@1395 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

---
+++ modules/network/port_scanner/module.rb
@@ -0,0 +1,42 @@
+#
+#   Copyright 2011 Wade Alcorn wade@bindshell.net
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#
+# Port Scanner Module - javier.marcos
+# Scan ports in a given hostname, using WebSockets CORS and HTTP with img tags. 
+# It uses the three methods to avoid blocked ports or Same Origin Policy.
+
+
+class Port_scanner < BeEF::Core::Command
+  
+  def self.options
+    return [
+        {'name' => 'ipHost', 'ui_label' => 'Scan IP or Hostname', 'value' => '192.168.1.10'},
+        {'name' => 'ports' , 'ui_label' => 'Specific port(s) to scan', 'value' => 'default'},
+	{'name' => 'closetimeout' , 'ui_label' => 'Closed port timeout (ms)', 'value' => '1100'},
+        {'name' => 'opentimeout', 'ui_label' => 'Open port timeout (ms)', 'value' => '2500'},
+        {'name' => 'delay', 'ui_label' => 'Delay between requests (ms)', 'value' => '200'}
+    ]
+  end
+  
+  def post_execute
+    content = {}
+    content['port'] =@datastore['port'] if not @datastore['port'].nil?
+    if content.empty?
+      content['fail'] = 'No opened ports have been found.'
+    end
+    save content
+  end
+end
