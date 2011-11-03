5c1a7c23d517fd5fe007389b6551eb56ee5b7b1f
antisnatchor <antisnatchor@b87d56ec-f9c0-11de-8c8a-61c5e9addfc9>
2011-11-03 09:39:00 +0000

    PortScanner module: added debug option in module config, added new common service ports.
    git-svn-id: https://beef.googlecode.com/svn/trunk@1403 b87d56ec-f9c0-11de-8c8a-61c5e9addfc9

--- modules/network/port_scanner/module.rb
+++ modules/network/port_scanner/module.rb
@@ -25,9 +25,10 @@ class Port_scanner < BeEF::Core::Command
     return [
         {'name' => 'ipHost', 'ui_label' => 'Scan IP or Hostname', 'value' => '192.168.1.10'},
         {'name' => 'ports' , 'ui_label' => 'Specific port(s) to scan', 'value' => 'default'},
-	{'name' => 'closetimeout' , 'ui_label' => 'Closed port timeout (ms)', 'value' => '1100'},
+	      {'name' => 'closetimeout' , 'ui_label' => 'Closed port timeout (ms)', 'value' => '1100'},
         {'name' => 'opentimeout', 'ui_label' => 'Open port timeout (ms)', 'value' => '2500'},
-        {'name' => 'delay', 'ui_label' => 'Delay between requests (ms)', 'value' => '200'}
+        {'name' => 'delay', 'ui_label' => 'Delay between requests (ms)', 'value' => '200'},
+        {'name' => 'debug', 'ui_label' => 'Debug', 'value' => 'false'}
     ]
   end
   
@@ -35,7 +36,7 @@ class Port_scanner < BeEF::Core::Command
     content = {}
     content['port'] =@datastore['port'] if not @datastore['port'].nil?
     if content.empty?
-      content['fail'] = 'No opened ports have been found.'
+      content['fail'] = 'No open ports have been found.'
     end
     save content
   end
