7eafb477c2b2fadace35846491e14bd0dc2cec5d
Scott Brown <scotty.b.brown@gmail.com>
2011-12-08 10:03:33 +1000

    Testing Jenkins auto test setup - forced failure.

--- test/unit/core/filter/tc_command.rb
+++ test/unit/core/filter/tc_command.rb
@@ -22,7 +22,7 @@ class TC_Filter < Test::Unit::TestCase
   
   def test_has_valid_param_chars
     assert((not BeEF::Filters::has_valid_param_chars?(nil)), 'Nil')
-    assert((not BeEF::Filters::has_valid_param_chars?("")), 'Empty string')
+    assert((not BeEF::Filters::has_valid_param_chars?("xx")), 'Empty string')
     assert((BeEF::Filters::has_valid_param_chars?("A")), 'Single char')
     assert((not BeEF::Filters::has_valid_param_chars?("+")), 'Single invalid char')
   end
