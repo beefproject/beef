999802ad69ae53f0ff805e6979b305e403271f1a
Wade Alcorn <wade@bindshell.net>
2011-12-30 21:54:43 +1000

    Added ignore to the eval grep tests.

--- test/unit/tc_grep.rb
+++ test/unit/tc_grep.rb
@@ -21,6 +21,7 @@ class TC_Grep < Test::Unit::TestCase
     Dir['../../**/*.rb'].each do |path|
       File.open( path ) do |f|
         next if /tc_grep.rb/.match(path) # skip this file
+        next if /\/msf-test\//.match(path) # skip this file
         f.grep( /\Weval\W/im ) do |line|
           assert(false, "Illegal use of 'eval' in framework: " + path + ':' + line)
         end
