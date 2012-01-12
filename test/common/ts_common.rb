cbaf8ab6b75d62101a8abe89566b4f46dd7eb4fb
Wade Alcorn <wade@bindshell.net>
2012-01-12 17:23:04 +1000

    Updated unit test structure

---
+++ test/common/ts_common.rb
@@ -0,0 +1,31 @@
+#
+#   Copyright 2012 Wade Alcorn wade@bindshell.net
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+# @note Version check to ensure BeEF is running Ruby 1.9 >
+if  RUBY_VERSION < '1.9'
+  puts "\n"
+  puts "Ruby version " + RUBY_VERSION + " is no longer supported. Please upgrade 1.9 or later."
+  puts "\n"
+  exit
+end
+
+begin
+  require 'test/unit/ui/console/testrunner'
+rescue LoadError
+  puts "The following instruction failed: require 'test/unit/ui/console/testrunner'"
+  puts "Please run: sudo gem install test-unit-full"
+  exit
+end
