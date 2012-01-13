57f0654b27f767b4b7617d6dd1db0716f34a0b36
Wade Alcorn <wade@bindshell.net>
2012-01-13 15:53:25 +1000

    Added login screen shot capture

--- test/integration/tc_login.rb
+++ test/integration/tc_login.rb
@@ -5,7 +5,9 @@ class TC_login < Test::Unit::TestCase
   def test_log_in
     session = Capybara::Session.new(:selenium)
     session.visit('http://localhost:3000/ui/panel')
+    session.driver.browser.save_screenshot("/tmp/login.png")
     session.has_content?('BeEF Authentication')
+    session.driver.browser.save_screenshot("/tmp/login.png")
     session.fill_in 'user', :with => 'beef'
     session.fill_in 'pass', :with => 'beef'
     session.click_button('Login')
