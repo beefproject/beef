698e01bb834b9cee3439f42764148280b2714c9d
antisnatchor <antisnatchor@gmail.com>
2012-03-04 16:36:08 +0100

    reverted back test_contants definition.

--- test/common/test_constants.rb
+++ test/common/test_constants.rb
@@ -1,6 +1,6 @@
 BEEF_TEST_DIR = "/tmp/beef-test/"
 
-ATTACK_DOMAIN = "127.0.0.1"
-VICTIM_DOMAIN = "127.0.0.1"
+ATTACK_DOMAIN = "attacker.beefproject.com"
+VICTIM_DOMAIN = "attacker.beefproject.com"
 ATTACK_URL = "http://" + ATTACK_DOMAIN + ":3000/ui/panel"
 VICTIM_URL = "http://" + VICTIM_DOMAIN + ":3000/demos/basic.html"
