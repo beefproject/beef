e1134e2fa691627645f3b0e138754cc929ee78e2
Graziano Felline <scream1988@gmail.com>
2012-04-07 14:01:37 +0200

    Thread for websocket

--- core/main/network_stack/websocket/websocket.rb
+++ core/main/network_stack/websocket/websocket.rb
@@ -26,8 +26,8 @@ module BeEF
           #todo antisnatchor: start websocket secure if beef.http.websocket.secure == true
           server = WebSocketServer.new :accepted_domains => "0.0.0.0",
                                        :port => config.get("beef.http.websocket.port")
-
-          server.run() do |ws|
+          websThread = Thread.new  {
+                    server.run() do |ws|
             #@TODO debug print the path and who request for hooked browser mapping
             print_info("Path requested #{ws.path} Origins #{ws.origin}")
             if ws.path == "/"
@@ -45,7 +45,8 @@ module BeEF
 
               end
             end
-        end
+                    end
+                    }
       end
     end
   end
