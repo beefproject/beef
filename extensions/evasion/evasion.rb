aa8e073494ceab3f0b9cb0b901df926cfc8005ed
antisnatchor <antisnatchor@gmail.com>
2012-06-21 12:34:26 +0100

    Changes debug logs in the evasion main class.

--- extensions/evasion/evasion.rb
+++ extensions/evasion/evasion.rb
@@ -37,10 +37,10 @@ module BeEF
               #2. call the "execute" method of the ruby module, passing the input
               #3. update the input in order that next technique will work on the pre-processed input.
               if File.exists?("#{$root_dir}/extensions/evasion/obfuscation/#{technique}.rb")
-                print_debug "[OBFUSCATION] Applying technique [#{technique}]"
                 klass = BeEF::Extension::Evasion.const_get(technique.capitalize).instance
                 is_bootstrap_needed = klass.need_bootstrap
                 if is_bootstrap_needed
+                  print_debug "[OBFUSCATION] Adding bootstrapper for technique [#{technique}]"
                   @bootstrap += klass.get_bootstrap
                 end
               end
