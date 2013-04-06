2fcdf1038dbbbeda35df1be1a9e388d5c313a5e5
Christian Frichot <xntrik@gmail.com>
2013-04-06 14:32:51 +0800

    xntriks updates to webcam_perm_check

--- modules/browser/webcam_permision_check/module.rb
+++ modules/browser/webcam_permision_check/module.rb
@@ -4,14 +4,15 @@
 # See the file 'doc/COPYING' for copying permission
 #
 
-class Wb < BeEF::Core::Command
+class Webcam_permission_check < BeEF::Core::Command
   def pre_send
-      BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.bind('/modules/browser/wb/cameraCheck.swf', '/cameraCheck', 'swf')
-      BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.bind('/modules/browser/wb/swfobject.js', '/swfobject', 'js')
+      BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.bind('/modules/browser/webcam_permission_check/cameraCheck.swf', '/cameraCheck', 'swf')
+      BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.bind('/modules/browser/webcam_permission_check/swfobject.js', '/swfobject', 'js')
   end
   
   def post_execute 
-    BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.unbind('/cameraCheck.swf')
+
+  	BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.unbind('/cameraCheck.swf')
 	BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.unbind('/swfobject.js')
   end
 
