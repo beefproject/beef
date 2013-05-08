d24a00a6399caf51bdd6f0cb6f137183a8fbb6a4
soh_cah_toa <soh_cah_toa@cybercrud.net>
2013-05-07 23:59:27 -0400

    Overrode RubyDNS::Transaction.respond! to use debug logger instead. Now all RubyDNS output is properly disabled unless --verbose is given.

--- extensions/dns/ruby/rubydns.rb
+++ extensions/dns/ruby/rubydns.rb
@@ -5,7 +5,7 @@
 #
 module RubyDNS
 
-  # Overrides RubyDNS::run_server() to behave exactly the same, minus the output
+  # Behaves exactly the same, minus the output
   def self.run_server(options = {}, &block)
     server = RubyDNS::Server.new(&block)
 
@@ -26,6 +26,26 @@ module RubyDNS
     end
 
     server.fire(:stop)
+  end
+
+  class Transaction
+
+    # Behaves exactly the same, except using debug logger instead of info
+    def respond!(*data)
+      options = data.last.kind_of?(Hash) ? data.pop : {}
+      resource_class = options[:resource_class] || @resource_class
+			
+      if resource_class == nil
+        raise ArgumentError, "Could not instantiate resource #{resource_class}!"
+      end
+			
+      @server.logger.debug "Resource class: #{resource_class.inspect}"
+      resource = resource_class.new(*data)
+      @server.logger.debug "Resource: #{resource.inspect}"
+			
+      append!(resource, options)
+    end
+
   end
 
 end
