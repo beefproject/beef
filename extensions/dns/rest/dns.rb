479ae4674726c5a95f853a559826647e1985365e
soh_cah_toa <soh_cah_toa@cybercrud.net>
2013-05-17 23:02:40 -0400

    Added RESTful API route for /api/dns/rule/:id. This will return a single rule given its unique id.

--- extensions/dns/rest/dns.rb
+++ extensions/dns/rest/dns.rb
@@ -3,45 +3,18 @@
 # Browser Exploitation Framework (BeEF) - http://beefproject.com
 # See the file 'doc/COPYING' for copying permission
 #
-
-# GET:
-#   * Rule count
-#   * List of rules
-
-# POST:
-#   * Add rule
-#   * Remove rule
-
-
-# /api/dns/rules
-# {
-#   "rules": [
-#       {
-#           "id": 1,
-#           "pattern": "foobar.com",
-#           "type": "Resolv::DNS::Resource::IN::A"
-#           "block": "proc {|t| ...do shit... }"
-#       },
-#
-#       {
-#       },
-#
-#       {
-#       },
-#   ]
-# }
-
-
-
 module BeEF
 module Extension
 module DNS
 
+  # This class handles the routing of RESTful API requests that query BeEF's DNS server
   class DNSRest < BeEF::Core::Router::Router
 
+    # Filters out bad requests before performing any routing
     before do
       config = BeEF::Core::Configuration.instance
 
+      # Require a valid API token from a valid IP address
       error 401 unless params[:token] == config.get('beef.api_token')
       halt  401 unless BeEF::Core::Rest.permitted_source?(request.ip)
 
@@ -53,8 +26,17 @@ module DNS
 
     # Returns the entire current DNS ruleset
     get '/rules' do
-      result = {}
-      result[:rules] = BeEF::Extension::DNS::DNS.instance.get_rules
+      result[:rules] = BeEF::Extension::DNS::DNS.instance.get_ruleset
+      result.to_json
+    end
+
+    # Returns a specific rule given its id
+    get '/rule/:id' do
+      id = params[:id]
+
+      halt 401 unless BeEF::Filters.nums_only?(id)
+
+      result = BeEF::Extension::DNS::DNS.instance.get_rule(id)
       result.to_json
     end
 
