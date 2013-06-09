4ba06772c418a57977821c5d983690a4bb35c584
soh_cah_toa <soh_cah_toa@cybercrud.net>
2013-06-08 23:25:23 -0400

    Included broken DNS unit tests so others can help debug. Temporary 'dns' and 'dns_rest' Rake tasks make it easier to run tests.

--- Rakefile
+++ Rakefile
@@ -32,6 +32,22 @@ task :integration => ["install"] do
   Rake::Task['beef_stop'].invoke
 end
 
+# TODO: Remove task before comitting
+desc "Run DNS integration tests"
+task :dns_rest do
+  Rake::Task['beef_start'].invoke
+  sh "cd test/integration; ruby -W0 ts_dns_rest.rb"
+  Rake::Task['beef_stop'].invoke
+end
+
+# TODO: Remove task before comitting
+desc "Run DNS unit tests"
+task :dns do
+  Rake::Task['beef_start'].invoke
+  sh "cd test/unit; ruby -W0 ts_unit_dns.rb"
+  Rake::Task['beef_stop'].invoke
+end
+
 desc "Run integration unit tests"
 task :unit => ["install"] do
   sh "cd test/unit;ruby -W0 ts_unit.rb"
--- test/unit/extensions/tc_dns.rb
+++ test/unit/extensions/tc_dns.rb
@@ -4,16 +4,39 @@
 # See the file 'doc/COPYING' for copying permission
 #
 require 'test/unit'
+require 'resolv'
 
 class TC_Dns < Test::Unit::TestCase
 
-  def setup
-    $:.unshift(File.join(File.expand_path(File.dirname(__FILE__)), '.'))
-    $root_dir = File.expand_path('../../../../', __FILE__)
+  class << self
+
+    def startup
+      $:.unshift(File.join(File.expand_path(File.dirname(__FILE__)), '../../../'))
+
+      require 'core/loader'
+      require 'extensions/dns/extension'
+
+      config = BeEF::Core::Configuration.instance
+
+      @@address = config.get('beef.extension.dns.address')
+      @@port    = config.get('beef.extension.dns.port')
+
+      Thread.new do
+        dns = BeEF::Extension::Dns::Server.instance
+        dns.run_server(@@address, @@port)
+      end
+    end
+
   end
 
-  def test_dns
-    assert(true)
+  def test_add_rule
+    dns = BeEF::Extension::Dns::Server.instance
+
+    id = dns.add_rule('foo.bar', Resolv::DNS::Resource::IN::A) do |transaction|
+      transaction.respond!('1.2.3.4')
+    end
+
+    assert_not_nil(id)
   end
 
 end
