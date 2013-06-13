a2b627c8ae411523a3e84e15ccdc4edd2172bd87
Nbblrr <Nbblrr>
2013-06-14 00:07:00 +0200

    Add module to detect bitdefender 2012. Closes #902

---
+++ modules/host/detect_bitdefender2012/module.rb
@@ -0,0 +1,13 @@
+#
+# Copyright (c) 2006-2013 Wade Alcorn - wade@bindshell.net
+# Browser Exploitation Framework (BeEF) - http://beefproject.com
+# See the file 'doc/COPYING' for copying permission
+#
+
+class Detect_bitdefender2012 < BeEF::Core::Command
+
+  def post_execute
+    save({'BitDefender' => @datastore['bitdefender']})
+  end
+
+end
