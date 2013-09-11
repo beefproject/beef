25aca3d291a899459479abbf612b3543ccb82948
bcoles <bcoles@gmail.com>
2013-09-11 15:26:15 +0930

    Update 'command.js' for Spyder Eye module

--- modules/browser/spyder_eye/command.js
+++ modules/browser/spyder_eye/command.js
@@ -1,4 +1,8 @@
-
+//
+// Copyright (c) 2006-2013 Wade Alcorn - wade@bindshell.net
+// Browser Exploitation Framework (BeEF) - http://beefproject.com
+// See the file 'doc/COPYING' for copying permission
+//
 
 beef.execute(function() {
 
