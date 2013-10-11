09443675cc4799dfc6553246ef321c36d64902d4
bcoles <bcoles@gmail.com>
2013-10-12 00:43:54 +1030

    Fix bug in fake_notification_ff module

--- modules/social_engineering/fake_notification_ff/module.rb
+++ modules/social_engineering/fake_notification_ff/module.rb
@@ -7,9 +7,10 @@ class Fake_notification_ff < BeEF::Core::Command
 
   def self.options
     @configuration = BeEF::Core::Configuration.instance
-    proto = configuration.get("beef.http.https.enable") == true ? "https" : "http"
-    beef_host = @configuration.get("beef.http.public") || @configuration.get("beef.http.host")
-    url = proto + '://' + beef_host + ':' + @configuration.get("beef.http.port") +'/api/ipec/ff_extension'
+    proto = @configuration.get("beef.http.https.enable") == true ? "https" : "http"
+    beef_host = @configuration.get("beef.http.public")      || @configuration.get("beef.http.host")
+    beef_port = @configuration.get("beef.http.public_port") || @configuration.get("beef.http.port")
+    url = "#{proto}://#{beef_host}:#{beef_port}/api/ipec/ff_extension"
     return [
       {'name' => 'url', 'ui_label' => 'Plugin URL', 'value' => url, 'width'=>'150px'},
       { 'name' => 'notification_text',
