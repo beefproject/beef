05502a3c91b7893553548df56b641c4206938d1e
bcoles <bcoles@gmail.com>
2013-11-04 15:52:54 +1030

    fix bug preventing loading of 'replace_video_fake_plugin' module

--- modules/social_engineering/replace_video_fake_plugin/module.rb
+++ modules/social_engineering/replace_video_fake_plugin/module.rb
@@ -6,10 +6,11 @@
 class Replace_video_fake_plugin < BeEF::Core::Command
 
   def self.options
-    @configuration = BeEF::Core::Configuration.instance
+    configuration = BeEF::Core::Configuration.instance
     proto = configuration.get("beef.http.https.enable") == true ? "https" : "http"
-    beef_host = @configuration.get("beef.http.public") || @configuration.get("beef.http.host")
-    url = proto + '://' + beef_host + ':' + @configuration.get("beef.http.port")
+    beef_host = configuration.get("beef.http.public")      || configuration.get("beef.http.host")
+    beef_port = configuration.get("beef.http.public_port") || configuration.get("beef.http.port")
+    url = "#{proto}://#{beef_host}:#{beef_port}"
     return [
         {'name' => 'url', 'ui_label' => 'Plugin URL', 'value' => url+'/api/ipec/ff_extension', 'width'=>'150px'},
         {'name' => 'jquery_selector', 'ui_label' => 'jQuery Selector', 'value' => 'embed', 'width'=>'150px'}
