c9f0b731001de4bd13bd7469ab0b9a31c56d3ff2
bcoles <bcoles@gmail.com>
2013-12-30 05:27:27 +1030

    Add config path to error message

--- core/main/configuration.rb
+++ core/main/configuration.rb
@@ -22,7 +22,7 @@ module BeEF
       # @param [String] configuration_file Configuration file to be loaded, by default loads $root_dir/config.yaml
       def initialize(config)
         raise Exception::TypeError, '"config" needs to be a string' if not config.string?
-        raise Exception::TypeError, 'Configuration yaml cannot be found' if not File.exist?(config)
+        raise Exception::TypeError, "Configuration file '#{config}' cannot be found" if not File.exist?(config)
         begin
           #open base config
           @config = self.load(config)
