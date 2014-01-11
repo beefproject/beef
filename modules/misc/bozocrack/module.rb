e50d681a647e68c2fb5b8465f3afdd664a52b419
bcoles <bcoles@gmail.com>
2014-01-11 23:27:13 +1030

    Add BozoCrack module

---
+++ modules/misc/bozocrack/module.rb
@@ -0,0 +1,22 @@
+#
+# Copyright (c) 2006-2014 Wade Alcorn - wade@bindshell.net
+# Browser Exploitation Framework (BeEF) - http://beefproject.com
+# See the file 'doc/COPYING' for copying permission
+#
+class Bozo_crack < BeEF::Core::Command
+
+	def self.options
+		return [
+			{'name' => 'hash', 'ui_label' => 'MD5 Hash', 'value' => '5f4dcc3b5aa765d61d8327deb882cf99' }
+		]
+	end
+
+	def post_execute
+		content = {}
+		content['result'] = @datastore['result'] if not @datastore['result'].nil?
+		content['fail']   = @datastore['fail']   if not @datastore['fail'].nil?
+		save content
+	end
+
+end
+
