0e57fb0be1f0f6d91438316490203c3f613b8d94
bmantra <bart.leppens@gmail.com>
2014-01-19 19:58:14 +0100

    Inital version of CookieJar overflow module.  And minor bugfix of active fax overflow module.

---
+++ modules/browser/hooked_domain/overflow_cookiejar/module.rb
@@ -0,0 +1,16 @@
+#
+# Copyright (c) 2006-2014 Wade Alcorn - wade@bindshell.net
+# Browser Exploitation Framework (BeEF) - http://beefproject.com
+# See the file 'doc/COPYING' for copying permission
+#
+class Overflow_cookiejar < BeEF::Core::Command
+  def self.options
+     return [
+	{'name' => 'preserveCookies', 'type' => 'checkbox', 'ui_label' => 'Attempt to preserve all non-httpOnly cookies', 'checked' => 'true'}
+      ]
+  end
+  def post_execute
+     save({'result' => @datastore['result']})
+  end
+
+end
