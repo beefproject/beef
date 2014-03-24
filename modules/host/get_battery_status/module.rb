6044de5604398cd6c9bb1ca375a751bbd23ae934
ecneladis <ecneladis@gmail.com>
2014-03-24 12:46:43 +0100

    Adding module for getting battery status

---
+++ modules/host/get_battery_status/module.rb
@@ -0,0 +1,17 @@
+#
+# Copyright (c) 2006-2014 Wade Alcorn - wade@bindshell.net
+# Browser Exploitation Framework (BeEF) - http://beefproject.com
+# See the file 'doc/COPYING' for copying permission
+#
+class Get_battery_status < BeEF::Core::Command
+
+  def post_execute
+    content = {}
+    content['chargingStatus'] = @datastore['chargingStatus']
+    content['batteryLevel'] = @datastore['batteryLevel']
+    content['chargingTime'] = @datastore['chargingTime']
+    content['dischargingTime'] = @datastore['dischargingTime']
+    save content
+  end
+
+end
