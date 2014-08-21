8ccc8f8b2e3bf5568d1b26cee910849aa3cf970c
Sergey Gorbaty <sergey.gorbaty@sgorbaty-ltm.internal.salesforce.com>
2014-08-21 11:20:19 -0700

    New alert user module

---
+++ modules/phonegap/phonegap_alert_user/module.rb
@@ -0,0 +1,39 @@
+#
+# Copyright (c) 2006-2014 Wade Alcorn - wade@bindshell.net
+# Browser Exploitation Framework (BeEF) - http://beefproject.com
+# See the file 'doc/COPYING' for copying permission
+#
+# Phonegap_prompt_user
+#
+
+class Phonegap_alert_user < BeEF::Core::Command
+  
+    def self.options
+        return [{
+            'name' => 'title', 
+            'description' => 'Alert title', 
+            'ui_label'=>'Title', 
+            'value' => 'Beef',
+            'width' => '300px'
+            },{
+            'name' => 'message', 
+            'description' => 'Message', 
+            'ui_label'=>'Message', 
+            'value' => 'Game over!',
+            'width' => '300px'
+            },{
+            'name' => 'buttonName',
+            'description' => 'Default button name',
+            'ui_label'=>'Button name',
+            'value' => 'Done',
+            'width' => '100px'
+            }]
+  end
+
+  def callback
+    content = {}
+    content['Result'] = @datastore['result']
+    save content
+  end 
+
+end
