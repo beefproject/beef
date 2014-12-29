2fbca61368ec32850fe7eead043cc327b384f174
Wade Alcorn <wade@bindshell.net>
2014-12-30 07:44:56 +1000

    Updated copyright dates

--- modules/persistence/man_in_the_browser/command.js
+++ modules/persistence/man_in_the_browser/command.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2014 Wade Alcorn - wade@bindshell.net
+ * Copyright (c) 2006-2015 Wade Alcorn - wade@bindshell.net
  * Browser Exploitation Framework (BeEF) - http://beefproject.com
  * See the file 'doc/COPYING' for copying permission
  */
