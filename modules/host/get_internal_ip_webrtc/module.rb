7e3ee5fc500f2480d2f273b2e901955c30cf7b23
Brendan Coles <bcoles@gmail.com>
2015-02-01 02:06:52 +0000

    Record OS

--- modules/host/get_internal_ip_webrtc/module.rb
+++ modules/host/get_internal_ip_webrtc/module.rb
@@ -20,11 +20,12 @@ class Get_internal_ip_webrtc < BeEF::Core::Command
       if @datastore['results'] =~ /IP is ([\d\.,]+)/
         ips = $1.to_s.split(/,/)
         if !ips.nil? && !ips.empty?
+          os = BeEF::Core::Models::BrowserDetails.get(session_id, 'OsName')
           ips.uniq.each do |ip|
             next unless ip =~ /^[\d\.]+$/
             next if ip =~ /^0\.0\.0\.0$/
             print_debug("Hooked browser has network interface #{ip}")
-            r = BeEF::Core::Models::NetworkHost.new(:hooked_browser_id => session_id, :ip => ip, :cid => cid)
+            r = BeEF::Core::Models::NetworkHost.new(:hooked_browser_id => session_id, :ip => ip, :os => os, :cid => cid)
             r.save
           end
         end
