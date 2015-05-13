dad3be1b9176deccbe90a2469cf8952092c9fadd
Brendan Coles <bcoles@gmail.com>
2015-05-14 07:14:35 +1000

    replace backticks with IO.open

--- test/integration/tc_dns_rest.rb
+++ test/integration/tc_dns_rest.rb
@@ -381,8 +381,7 @@ class TC_DnsRest < Test::Unit::TestCase
     address = @@config.get('beef.extension.dns.address')
     port = @@config.get('beef.extension.dns.port')
 
-    #TODO antisnatchor: dangerous, replace with IO.open([param_array]) as in web_cloner.rb
-    dig_output = `dig @#{address} -p #{port} -t #{type} #{pattern}`
+    dig_output = IO.popen(["dig", "@#{address}", "-p", "#{port}", "-t", "#{type}", "#{pattern}"], 'r+').read
     assert_match(regex, dig_output)
   end
 
