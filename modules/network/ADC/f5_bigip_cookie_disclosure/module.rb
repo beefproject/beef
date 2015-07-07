d737bb19c28c1fa32e20aa4492b76897b5dacb21
Brendan Coles <bcoles@gmail.com>
2015-07-07 14:51:04 +0000

    save results

--- modules/network/ADC/f5_bigip_cookie_disclosure/module.rb
+++ modules/network/ADC/f5_bigip_cookie_disclosure/module.rb
@@ -6,8 +6,8 @@
 class F5_bigip_cookie_disclosure < BeEF::Core::Command
 
   def post_execute
-    return if @datastore['result'].nil?
-    save({'BigIPCookie' => @datastore['BigIPCookie']})
+    return if @datastore['results'].nil?
+    save({'BigIPCookie' => @datastore['results']})
   end
 
 end
