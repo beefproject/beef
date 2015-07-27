a2d4def9835ebcdef4ac2f9f75cc076bd834253a
antisnatchor <antisnatchor@gmail.com>
2015-07-27 10:45:35 +0200

    Added readme for ARE enabled rules folder, removed jools test stuff

--- test/integration/ts_integration.rb
+++ test/integration/ts_integration.rb
@@ -15,7 +15,7 @@ require 'selenium/webdriver'
 require './check_environment' # Basic log in and log out tests
 require './tc_debug_modules' # RESTful API tests (as well as debug modules)
 require './tc_login' # Basic log in and log out tests
-require './tc_jools' # Basic tests for jools
+#require './tc_jools' # Basic tests for jools
 require './tc_network_rest' # Basic tests for Network extension RESTful API interface
 #require './tc_dns_rest' # Basic tests for DNS RESTful API interface
 require './tc_social_engineering_rest' # Basic tests for social engineering RESTful API interface
@@ -28,7 +28,7 @@ class TS_BeefIntegrationTests
     suite << TC_CheckEnvironment.suite
     suite << TC_Login.suite
     suite << TC_DebugModules.suite
-    suite << TC_Jools.suite
+    #suite << TC_Jools.suite
     suite << TC_NetworkRest.suite
     #suite << TC_DnsRest.suite
     suite << TC_SocialEngineeringRest.suite
