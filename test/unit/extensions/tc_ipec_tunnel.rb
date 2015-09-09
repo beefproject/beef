fe8aacecf48320748257c13ecca9b538f96b7902
Brendan Coles <bcoles@gmail.com>
2015-09-09 18:51:40 +0000

    Add Ipec unit test stub - Fix #586

--- test/unit/extensions/tc_ipec_tunnel.rb
+++ test/unit/extensions/tc_ipec_tunnel.rb
@@ -7,13 +7,43 @@ require 'test/unit'
 
 class TC_IpecTunnel < Test::Unit::TestCase
 
-  def setup
-    $:.unshift(File.join(File.expand_path(File.dirname(__FILE__)), '.'))
-    $root_dir = File.expand_path('../../../../', __FILE__)
+  class << self
+
+    def startup
+      $root_dir = '../../'
+      $:.unshift(File.expand_path($root_dir))
+
+      # load extension
+      require 'extensions/ipec/extension'
+
+      # load config
+      BeEF::Core::Configuration.new(File.join($root_dir, 'config.yaml'))
+      config = BeEF::Core::Configuration.instance
+      config.load_extensions_config
+      @@ipec_config = config.get('beef.extension.ipec')
+    end
+
+    def shutdown
+      $root_dir = nil
+    end
+
   end
 
-  def test_ipec_tunnel
-    assert(true)
+  # Connects to in-memory database (does not test anything)
+  def test_01_database
+    DataMapper.setup(:default, 'sqlite3::memory:')
+    DataMapper.auto_migrate!
+  end
+
+  # Checks for required settings in config file
+  def test_02_config
+    assert(@@ipec_config.has_key?('enable'))
+  end
+
+  # Verifies public interface
+  def test_03_interface
+    @@ipec_junk_calc = BeEF::Extension::Ipec::JunkCalculator.instance
+    assert_respond_to(@@ipec_junk_calc, :bind_junk_calculator)
   end
 
 end
