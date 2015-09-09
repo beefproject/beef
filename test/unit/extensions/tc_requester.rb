44c5c73f97476e360382ae58c81ced6511e14960
Brendan Coles <bcoles@gmail.com>
2015-09-09 07:39:34 +0000

    Add Requester unit tests - Fix #29

--- test/unit/extensions/tc_requester.rb
+++ test/unit/extensions/tc_requester.rb
@@ -7,34 +7,45 @@ require 'test/unit'
 
 class TC_Requester < Test::Unit::TestCase
 
-  def setup
-    $:.unshift(File.join(File.expand_path(File.dirname(__FILE__)), '.'))
-    $root_dir = File.expand_path('../../../../', __FILE__)
+  class << self
+
+    def startup
+      $root_dir = '../../'
+      $:.unshift(File.expand_path($root_dir))
+
+      # load extension
+      require 'extensions/requester/extension'
+
+      # load config
+      BeEF::Core::Configuration.new(File.join($root_dir, 'config.yaml'))
+      config = BeEF::Core::Configuration.instance
+      config.load_extensions_config
+      @@requester_config = config.get('beef.extension.requester')
+    end
+
+    def shutdown
+      $root_dir = nil
+    end
+
   end
 
-  def test_requester
-    assert(true)
+  # Connects to in-memory database (does not test anything)
+  def test_01_database
+    DataMapper.setup(:default, 'sqlite3::memory:')
+    DataMapper.auto_migrate!
   end
-  def test_delete
-    assert(true)
+
+  # Checks for required settings in config file
+  def test_02_config
+    assert(@@requester_config.has_key?('enable'))
   end
-  def test_put
-    assert(true)
-  end
-  def test_head
-    assert(true)
-  end
-  def test_no_params
-    assert(true)
-  end
-  def test_zero_values
-    assert(true)
-  end
-  def test_one_values
-    assert(true)
-  end
-  def test_neg_one_values
-    assert(true)
+
+  # Verifies public interface
+  def test_03_interface
+    @@requester = BeEF::Extension::Requester::API::Hook.new
+    assert_respond_to(@@requester, :requester_run)
+    assert_respond_to(@@requester, :add_to_body)
+    assert_respond_to(@@requester, :requester_parse_db_request)
   end
 
 end
