9a0a36176ff72a7f285947bb28b3a22ead9f53c4
Brendan Coles <bcoles@gmail.com>
2015-09-09 07:41:56 +0000

    Add Proxy unit tests - Fix #30

--- test/unit/extensions/tc_proxy.rb
+++ test/unit/extensions/tc_proxy.rb
@@ -7,34 +7,41 @@ require 'test/unit'
 
 class TC_Proxy < Test::Unit::TestCase
 
-  def setup
-    $:.unshift(File.join(File.expand_path(File.dirname(__FILE__)), '.'))
-    $root_dir = File.expand_path('../../../../', __FILE__)
+  class << self
+
+    def startup
+      $root_dir = '../../'
+      $:.unshift(File.expand_path($root_dir))
+
+      # load extension
+      require 'extensions/proxy/extension'
+
+      # load config
+      BeEF::Core::Configuration.new(File.join($root_dir, 'config.yaml'))
+      config = BeEF::Core::Configuration.instance
+      config.load_extensions_config
+      @@proxy_config = config.get('beef.extension.proxy')
+    end
+
+    def shutdown
+      $root_dir = nil
+    end
+
   end
 
-  def test_proxy
-    assert(true)
+  # Connects to in-memory database (does not test anything)
+  def test_01_database
+    DataMapper.setup(:default, 'sqlite3::memory:')
+    DataMapper.auto_migrate!
   end
-  def test_delete
-    assert(true)
-  end
-  def test_put
-    assert(true)
-  end
-  def test_head
-    assert(true)
-  end
-  def test_no_params
-    assert(true)
-  end
-  def test_zero_values
-    assert(true)
-  end
-  def test_one_values
-    assert(true)
-  end
-  def test_neg_one_values
-    assert(true)
+
+  # Checks for required settings in config file
+  def test_02_config
+    assert(@@proxy_config.has_key?('enable'))
+    assert(@@proxy_config.has_key?('address'))
+    assert(@@proxy_config.has_key?('port'))
+    assert(@@proxy_config.has_key?('key'))
+    assert(@@proxy_config.has_key?('cert'))
   end
 
 end
