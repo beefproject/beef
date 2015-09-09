091841fc1a6a8f5e65e71fa7000c8e8136b00c2f
Brendan Coles <bcoles@gmail.com>
2015-09-09 18:47:11 +0000

    Add QRcode extension unit test stub

--- test/unit/ts_unit.rb
+++ test/unit/ts_unit.rb
@@ -27,6 +27,7 @@ require './extensions/tc_proxy'
 require './extensions/tc_requester'
 require './extensions/tc_event_logger'
 require './extensions/tc_network'
+require './extensions/tc_qrcode'
 #require './extensions/tc_dns'
 require './tc_grep'
 require './tc_filesystem'
@@ -53,6 +54,7 @@ class TS_BeefTests
     suite << TC_Proxy.suite
     suite << TC_EventLogger.suite
     suite << TC_Network.suite
+    suite << TC_Qrcode.suite
     suite << TC_Hooks.suite
     suite << TC_Redirector.suite
     suite << TC_DynamicReconstruction.suite
