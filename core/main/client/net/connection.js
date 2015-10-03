9386fa28e7eb47116a9d48e0890ca1acd6040d93
Brendan Coles <bcoles@gmail.com>
2015-10-03 14:27:27 +0000

    Add beef.net.connection client methods

---
+++ core/main/client/net/connection.js
@@ -0,0 +1,47 @@
+//
+// Copyright (c) 2006-2015 Wade Alcorn - wade@bindshell.net
+// Browser Exploitation Framework (BeEF) - http://beefproject.com
+// See the file 'doc/COPYING' for copying permission
+//
+
+// beef.net.connection - wraps Mozilla's Network Information API
+// https://developer.mozilla.org/en-US/docs/Web/API/NetworkInformation
+// https://developer.mozilla.org/en-US/docs/Web/API/Navigator/connection
+beef.net.connection = {
+
+  /* Returns the connection type
+   * @example: beef.net.connection.type()
+   * @note: https://developer.mozilla.org/en-US/docs/Web/API/NetworkInformation/type
+   * @return: {String} connection type or 'unknown'.
+   **/
+  type: function () {
+    try {
+      var connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+      var type = connection.type;
+      if (type) return type; else return 'unknown';
+    } catch(e) {
+      beef.debug("Error retrieving connection type: " + e.message);
+      return 'unknown';
+    }
+  },
+
+  /* Returns the maximum downlink speed of the connection
+   * @example: beef.net.connection.downlinkMax()
+   * @note: https://developer.mozilla.org/en-US/docs/Web/API/NetworkInformation/downlinkMax
+   * @return: {String} downlink max or 'unknown'.
+   **/
+  downlinkMax: function () {
+    try {
+      var connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+      var max = connection.downlinkMax;
+      if (max) return max; else return 'unknown';
+    } catch(e) {
+      beef.debug("Error retrieving connection downlink max: " + e.message);
+      return 'unknown';
+    }
+  }
+
+};
+
+beef.regCmp('beef.net.connection');
+
