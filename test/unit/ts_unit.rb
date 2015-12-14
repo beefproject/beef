ebd2fa60886d1ecba1428920ae756d5be5fe469d
Brendan Coles <bcoles@gmail.com>
2015-12-14 16:57:31 +0000

    Add console unit test stub

--- test/unit/ts_unit.rb
+++ test/unit/ts_unit.rb
@@ -28,6 +28,7 @@ require './extensions/tc_requester'
 require './extensions/tc_event_logger'
 require './extensions/tc_network'
 require './extensions/tc_qrcode'
+require './extensions/tc_console'
 #require './extensions/tc_dns'
 require './tc_grep'
 require './tc_filesystem'
@@ -58,6 +59,7 @@ class TS_BeefTests
     suite << TC_Hooks.suite
     suite << TC_Redirector.suite
     suite << TC_DynamicReconstruction.suite
+    suite << TC_Console.suite
     #suite << TC_Dns.suite
 
     return suite
