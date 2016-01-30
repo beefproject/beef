9e7f36c113aaaec854b350435c1165a005d86995
Brendan Coles <bcoles@gmail.com>
2016-01-30 13:56:22 +0000

    Re-indent

--- extensions/admin_ui/constants/agents.rb
+++ extensions/admin_ui/constants/agents.rb
@@ -10,25 +10,25 @@ module Constants
   
   # The User Agent strings for browser detection
   module Agents
-  
+
     AGENT_UNKNOWN_IMG     = 'unknown.png'
-  	AGENT_FIREFOX_UA_STR  = 'Firefox'
-  	AGENT_FIREFOX_IMG     = 'firefox.png'
-  	AGENT_MOZILLA_UA_STR  = 'Mozilla'
-  	AGENT_MOZILLA_IMG     = 'mozilla.png'
-  	AGENT_IE_UA_STR       = 'MSIE'
-  	AGENT_IE_IMG          = 'msie.png'
-  	AGENT_SAFARI_UA_STR   = 'Safari'
-  	AGENT_SAFARI_IMG      = 'safari.png'
-  	AGENT_KONQ_UA_STR     = 'Konqueror'
-  	AGENT_KONQ_IMG        = 'konqueror.png'
-  	AGENT_CHROME_UA_STR   = 'Chrome'
-  	AGENT_CHROME_IMG      = 'chrome.png'
+    AGENT_FIREFOX_UA_STR  = 'Firefox'
+    AGENT_FIREFOX_IMG     = 'firefox.png'
+    AGENT_MOZILLA_UA_STR  = 'Mozilla'
+    AGENT_MOZILLA_IMG     = 'mozilla.png'
+    AGENT_IE_UA_STR       = 'MSIE'
+    AGENT_IE_IMG          = 'msie.png'
+    AGENT_SAFARI_UA_STR   = 'Safari'
+    AGENT_SAFARI_IMG      = 'safari.png'
+    AGENT_KONQ_UA_STR     = 'Konqueror'
+    AGENT_KONQ_IMG        = 'konqueror.png'
+    AGENT_CHROME_UA_STR   = 'Chrome'
+    AGENT_CHROME_IMG      = 'chrome.png'
     AGENT_OPERA_UA_STR    = 'Opera'
-  	AGENT_OPERA_IMG       = 'opera.ico'
-  
+    AGENT_OPERA_IMG       = 'opera.ico'
+
   end
-  
+
 end
 end
 end
