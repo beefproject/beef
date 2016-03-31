617a30f80e6e1485cd4802af6b21d8407aa125e1
mgeeky <mgeeky@users.noreply.github.com>
2016-03-31 12:25:27 +0200

    Added functionality to specify custom static resources paths for Get Visited Domains module.

--- modules/browser/get_visited_domains/module.rb
+++ modules/browser/get_visited_domains/module.rb
@@ -6,6 +6,18 @@
 
 class Get_visited_domains < BeEF::Core::Command
 
+  def self.options
+    return [{
+      'name' => 'domains', 
+      'description' => 'Specify additional resources to fetch during visited domains analysis. Paste to the below field full URLs leading to CSS, image, JS or other *static* resources hosted on desired page. Separate domain names with url by using semicolon (;). Next domains separate by comma (,).', 
+      'type' => 'textarea',
+      'ui_label' => 'Specify custom page to check',
+      'value' => 'Redtube ; http://images.cdn.redtube.com/_thumbs/v2009/favicon.ico,',
+      'width' => '400px',
+      'height' => '200px'
+    }]
+  end
+
   def post_execute
     content = {}
     content['results'] = @datastore['results']
