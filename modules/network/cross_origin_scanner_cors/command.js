7b96212876829965ddc0a3620e0ea3f46be6c7e7
Brendan Coles <bcoles@gmail.com>
2016-04-04 10:02:49 +0000

    Add wait option to CORS scanner

--- modules/network/cross_origin_scanner_cors/command.js
+++ modules/network/cross_origin_scanner_cors/command.js
@@ -9,9 +9,9 @@ beef.execute(function() {
   var ips = new Array();
   var ipRange = "<%= @ipRange %>";
   var ports   = "<%= @ports %>";
-  var threads = "<%= @threads %>";
-  var timeout = <%= @timeout %>*1000;
-  var wait = 2;
+  var threads = parseInt("<%= @threads %>", 10);
+  var timeout = parseInt("<%= @timeout %>", 10)*1000;
+  var wait    = parseInt("<%= @wait %>", 10)*1000;
 
   if(!beef.browser.hasCors()) {
     beef.net.send('<%= @command_url %>', <%= @command_id %>, 'fail=Browser does not support CORS', beef.are.status_error());
@@ -96,7 +96,7 @@ beef.execute(function() {
   // create worker queue
   var workers = new Array();
   for (w=0; w < threads; w++) {
-    workers.push(new WorkerQueue(wait*1000));
+    workers.push(new WorkerQueue(wait));
   }
 
   // send CORS request to each IP
