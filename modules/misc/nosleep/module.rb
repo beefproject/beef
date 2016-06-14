0234c548e5f1b99c45dbd888b07751e162ca251d
Brendan Coles <bcoles@gmail.com>
2016-06-14 19:30:56 +0000

    Add NoSleep module

---
+++ modules/misc/nosleep/module.rb
@@ -0,0 +1,22 @@
+#
+# Copyright (c) 2006-2016 Wade Alcorn - wade@bindshell.net
+# Browser Exploitation Framework (BeEF) - http://beefproject.com
+# See the file 'doc/COPYING' for copying permission
+#
+class No_sleep < BeEF::Core::Command
+  def pre_send
+    BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.bind('/modules/misc/nosleep/NoSleep.min.js','/NoSleep', 'js')
+  end 
+ 
+  def self.options
+    return [
+    ]
+  end
+  
+  def post_execute
+    content = {}
+    content['result'] = @datastore['result']
+    BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.unbind('NoSleep.js')
+    save content
+  end
+end
