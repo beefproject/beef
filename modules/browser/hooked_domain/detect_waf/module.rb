c3ae9e86b9692909270abdcc8f4476d4f99ed7fc
Pavel Lineytsev <lineycevp@gmail.com>
2016-08-14 12:15:04 +0700

    Added the ability to change the attack vector

--- modules/browser/hooked_domain/detect_waf/module.rb
+++ modules/browser/hooked_domain/detect_waf/module.rb
@@ -11,7 +11,11 @@ class Detect_waf < BeEF::Core::Command
     waf_url = "#{proto}://#{configuration.get("beef.http.host")}:#{configuration.get("beef.http.port")}/waf"
     return [
       { 'name' => 'targetUrl', 'ui_label' => 'Target URL', 'value' => 'http://testmywaf.com', 'width' => '200px' },
-      { 'name' => 'handlerUrl', 'ui_label' => 'Handler URL', 'value' => waf_url, 'width'=>'200px' }
+      { 'name' => 'handlerUrl', 'ui_label' => 'Handler URL', 'value' => waf_url, 'width'=>'200px' },
+      { 'name' => 'attackVector1', 'ui_label' => 'Vector 1', 'value' => 'cmd.exe', 'width'=>'200px' },
+      { 'name' => 'attackVector2', 'ui_label' => 'Vector 2', 'value' => '<script>alert(1)</script>', 'width'=>'200px' },
+      { 'name' => 'attackVector3', 'ui_label' => 'Vector 3', 'value' => '/Admin_Files/', 'width'=>'200px' },
+      { 'name' => 'attackVector4', 'ui_label' => 'Vector 4', 'value' => '../../../../etc/passwd', 'width'=>'200px' }
     ]
   end
 	
