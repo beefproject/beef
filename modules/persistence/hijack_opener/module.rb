49c3df1e44a64b7a6b2a0d543a31b6319bc5e625
Brendan Coles <bcoles@gmail.com>
2016-09-01 16:19:33 +0000

    Add Hijack Opener module

---
+++ modules/persistence/hijack_opener/module.rb
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2006-2016 Wade Alcorn - wade@bindshell.net
+# Browser Exploitation Framework (BeEF) - http://beefproject.com
+# See the file 'doc/COPYING' for copying permission
+#
+class Hijack_opener < BeEF::Core::Command
+  def post_execute
+    save({'result' => @datastore['result']})
+  end
+end
