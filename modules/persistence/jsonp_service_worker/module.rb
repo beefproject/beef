defdadb673f650ff28c628ba3ebd968e05c6c2f8
Claudio Contin <c.contin@aurainfosec.com>
2017-04-16 15:28:48 +1200

    encodeURIComponent instead of requiring user to input already encoded string use beef.net.hook instead of hook.js specify only chrome supported, and ALL others not

--- modules/persistence/jsonp_service_worker/module.rb
+++ modules/persistence/jsonp_service_worker/module.rb
@@ -7,7 +7,7 @@ class Jsonp_service_worker < BeEF::Core::Command
   def self.options
     return [
       {'name' => 'JSONPPath', 'ui_label' => 'Path of the current domain compromized JSONP endpoint (ex: /jsonp?callback=)', 'value' => '/jsonp?callback='},
-      {'name' => 'tempBody', 'ui_label' => 'Temporary HTML body to show to the users (ASCII HEX encoding needed)', 'value' => '%3Ch3%3EUnplanned%20site%20maintenance,%20please%20wait%20a%20few%20seconds,%20we%20are%20almost%20done.%3C%2Fh3%3E'}
+      {'name' => 'tempBody', 'ui_label' => 'Temporary HTML body to show to the users', 'value' => '<h3>Unplanned site maintenance, please wait a few seconds, we are almost done.</h3>'}
     ]
   end
 
