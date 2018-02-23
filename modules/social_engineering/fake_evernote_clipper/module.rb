9f7136f0ce587c5b9ae6dbc8f8e6b98a461890fe
Francois van der Hoven <francoisvanderhoven@gmail.com>
2018-02-23 10:15:27 +1000

    Add latest versions of js libraries

--- modules/social_engineering/fake_evernote_clipper/module.rb
+++ modules/social_engineering/fake_evernote_clipper/module.rb
@@ -4,7 +4,7 @@
 # See the file 'doc/COPYING' for copying permission
 #
 class Fake_evernote_clipper < BeEF::Core::Command
-  
+
   def pre_send
     BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.bind('/modules/social_engineering/fake_evernote_clipper/login.html','/ev/login','html')
     BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.bind('/modules/social_engineering/fake_evernote_clipper/login.css','/ev/login','css')
@@ -14,15 +14,15 @@ class Fake_evernote_clipper < BeEF::Core::Command
     BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.bind('/modules/social_engineering/fake_evernote_clipper/evernote_web_clipper.png','/ev/evernote_web_clipper','png')
     BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.bind('/modules/social_engineering/fake_evernote_clipper/GothamSSm-Medium.otf','/ev/GothamSSm-Medium','otf')
     BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.bind('/modules/social_engineering/fake_evernote_clipper/GothamSSm-Bold.otf','/ev/GothamSSm-Bold','otf')
-    BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.bind('/core/main/client/lib/jquery-1.12.4.min.js','/ev/jquery','js')
+    BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.bind('/core/main/client/lib/jquery-3.3.1.min.js','/ev/jquery','js')
   end
-  
+
   #
   # This method is being called when a zombie sends some
   # data back to the framework.
   #
   def post_execute
-    if (@datastore['meta'] == "KILLFRAME") 
+    if (@datastore['meta'] == "KILLFRAME")
       BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.unbind('/ev/login.html')
       BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.unbind('/ev/login.css')
       BeEF::Core::NetworkStack::Handlers::AssetHandler.instance.unbind('/ev/clipboard.png')
@@ -37,5 +37,5 @@ class Fake_evernote_clipper < BeEF::Core::Command
     content['result'] = @datastore['result']
     save content
   end
-  
+
 end
