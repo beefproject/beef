5bfd1e54df0c3f7b458d9cbb7f649027dd27a518
Ben Passmore <contact@passbe.com>
2019-10-09 10:04:17 +1000

    Added autorun model migrations

---
+++ core/main/ar-migrations/009_create_rules.rb
@@ -0,0 +1,20 @@
+class CreateRules < ActiveRecord::Migration[6.0]
+
+	def change
+
+		create_table :rules do |t|
+			t.text :name
+			t.text :author
+			t.text :browser
+			t.text :browser_version
+			t.text :os
+			t.text :os_version
+			t.text :modules
+			t.text :execution_order
+			t.text :execution_delay
+			t.text :chain_mode
+		end
+
+	end
+
+end
