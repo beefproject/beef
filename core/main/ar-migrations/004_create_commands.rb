aac1b0bc107c145e49f7baf42eb8a80e101badbb
Ben Passmore <contact@passbe.com>
2019-11-02 08:26:41 +1000

    Added command references

--- core/main/ar-migrations/004_create_commands.rb
+++ core/main/ar-migrations/004_create_commands.rb
@@ -3,6 +3,8 @@ class CreateCommands < ActiveRecord::Migration[6.0]
 	def change
 
 		create_table :commands do |t|
+            t.references :command_module
+            t.references :hooked_browser
 			t.text :data
 			t.datetime :creationdate
 			t.text :label
