cecbabe42a3f54c44cb7a923125e1ed051cab6a2
Haoxi Tan <haoxi.tan@gmail.com>
2020-01-16 11:29:59 +1000

    added test for fixing #1852

--- spec/beef/extensions/network_spec.rb
+++ spec/beef/extensions/network_spec.rb
@@ -3,11 +3,18 @@ require 'extensions/network/models/network_host'
 
 RSpec.describe 'BeEF Extension Network' do
 
-  it 'add good host' do
+  it 'add good local host' do
     expect {
       BeEF::Core::Models::NetworkHost.create(:hooked_browser_id => '1234', :ip => '127.0.0.1')
     }.to_not raise_error
     expect(BeEF::Core::Models::NetworkHost.where(hooked_browser_id: '1234', ip: '127.0.0.1')).to_not be_empty
+  end
+
+  it 'add good not local host' do
+    expect {
+      BeEF::Core::Models::NetworkHost.create(:hooked_browser_id => '12', :ip => '192.168.1.2')
+    }.to_not raise_error
+    expect(BeEF::Core::Models::NetworkHost.where(hooked_browser_id: '12', ip: '192.168.1.2')).to_not be_empty
   end
 
   it 'add good service' do
