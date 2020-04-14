a271d7656b55949c7a45e60c170f7cae3edd0146
Jack Walker <jwrc@protonmail.com>
2020-04-14 16:21:40 +1000

    Pushing changes that should enable BrowserStack. Partly pushing to see if we get these results in Travis.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,10 @@ require 'core/bootstrap.rb'
 require 'rack/test'
 require 'curb'
 require 'rest-client'
+require 'yaml'
+require 'selenium-webdriver'
+require 'browserstack/local'
+require 'byebug'
 
 # Require supports
 Dir['spec/support/*.rb'].each do |f|
@@ -21,6 +25,13 @@ end
 
 ENV['RACK_ENV'] ||= 'test'
 ARGV = []
+
+# BrowserStack config
+TASK_ID = (ENV['TASK_ID'] || 0).to_i
+CONFIG_NAME = ENV['CONFIG_NAME'] || 'browserstack'
+CONFIG = YAML.safe_load(File.read("./spec/support/#{CONFIG_NAME}.config.yml"))
+CONFIG['user'] = ENV['BROWSERSTACK_USERNAME'] || CONFIG['user']
+CONFIG['key'] = ENV['BROWSERSTACK_ACCESS_KEY'] || CONFIG['key']
 
 ActiveRecord::Base.logger = nil
 OTR::ActiveRecord.migrations_paths = [File.join('core', 'main', 'ar-migrations')]
@@ -44,6 +55,32 @@ RSpec.configure do |config|
     ActiveRecord::Base.transaction do
       example.run
       raise ActiveRecord::Rollback
+    end
+  end
+  # BrowserStack
+  config.around(:example) do |example|
+    @caps = CONFIG['common_caps'].merge(CONFIG['browser_caps'][TASK_ID])
+    @caps["name"] = ENV['name'] || example.metadata[:name] || example.metadata[:file_path].split('/').last.split('.').first
+    enable_local = @caps["browserstack.local"] && @caps["browserstack.local"].to_s == "true"
+
+    # Code to start browserstack local before start of test
+    if enable_local
+      @bs_local = BrowserStack::Local.new
+      bs_local_args = { "key" => CONFIG['key'], "forcelocal" => true }
+      @bs_local.start(bs_local_args)
+      @caps["browserstack.local"] = true
+    end
+
+    @driver = Selenium::WebDriver.for(:remote,
+      :url => "http://#{CONFIG['user']}:#{CONFIG['key']}@#{CONFIG['server']}/wd/hub",
+      :desired_capabilities => @caps)
+
+    begin
+      example.run
+    ensure 
+      @driver.quit
+      # Code to stop browserstack local after end of test
+      @bs_local.stop if enable_local
     end
   end
 end
