f82d4645d5969303a9f80bf80490b2ec99a40a95
Jack Walker <jwrc@protonmail.com>
2020-04-23 10:54:51 +1000

    Separated Selenium driver + BrowserStack code based on hook loadtime

--- spec/beef/modules/debug/test_beef_debugs_spec.rb
+++ spec/beef/modules/debug/test_beef_debugs_spec.rb
@@ -118,6 +118,8 @@ RSpec.describe 'BeEF Debug Command Modules:', :run_on_browserstack => true do
     end
 
     after(:all) do
+        puts @driver
+        puts @driver.methods.sort
         @driver.quit
 
         # Code to stop browserstack local after end of test
