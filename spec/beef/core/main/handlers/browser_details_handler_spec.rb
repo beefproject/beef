5658009f91c0d08bb26e53cb0d612865fe703893
Jack Walker <jwrc@protonmail.com>
2020-04-24 11:51:28 +1000

    eql -> eq

--- spec/beef/core/main/handlers/browser_details_handler_spec.rb
+++ spec/beef/core/main/handlers/browser_details_handler_spec.rb
@@ -113,6 +113,6 @@ RSpec.describe 'Browser Details Handler', :run_on_browserstack => true do
 		response = RestClient.get "#{RESTAPI_HOOKS}/#{@session}?token=#{@token}"
 		details = JSON.parse(response.body)
 
-		expect(@driver.browser.to_s.downcase).to eql (details['browser.name.friendly'].downcase) || eql ('internet_explorer') || eql ('msedge')
+		expect(@driver.browser.to_s.downcase).to eq (details['browser.name.friendly'].downcase) || eq ('internet_explorer') || eq ('msedge')
 	end
 end
