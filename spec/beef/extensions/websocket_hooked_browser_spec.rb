1ec3546532406616556450898d46bca682754b02
Josh <josh.crew@alcorngroup.com>
2020-04-27 21:23:29 -0700

    updated changes

--- spec/beef/extensions/websocket_hooked_browser_spec.rb
+++ spec/beef/extensions/websocket_hooked_browser_spec.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 #
 # Copyright (c) 2006-2020 Wade Alcorn - wade@bindshell.net
 # Browser Exploitation Framework (BeEF) - http://beefproject.com
@@ -7,122 +6,122 @@
 
 require 'rest-client'
 require 'json'
-require_relative '../../support/constants'
-require_relative '../../support/beef_test'
+require_relative '../../../../support/constants'
+require_relative '../../../../support/beef_test'
 require 'core/main/network_stack/websocket/websocket'
 require 'websocket-client-simple'
 
-RSpec.describe 'BeEF WebSockets: Browser Hooking', :run_on_browserstack => true do
-  before(:all) do
-      @config = BeEF::Core::Configuration.instance
-      p "This is the /n #@config "
-      @secure_port = @config.get('beef.http.websocket.secure_port')
-      p "This is the /n #@secure_port"
-      @config.set('beef.http.websocket.secure', true)
-      @config.set('beef.http.websocket.enable', true)
-      #set config parameters
-      @username = @config.get('beef.credentials.user')
-      p "This is the /n #@username "
-      @password = @config.get('beef.credentials.passwd')
-      p "This is the /n #@password "
-    
-		  # Load BeEF extensions and modules
-	  	# Always load Extensions, as previous changes to the config from other tests may affect
-		  # whether or not this test passes.
-	  	print_info "Loading in BeEF::Extensions"
-	  	BeEF::Extensions.load
-	  	sleep 2
+RSpec.describe 'Browser hooking with Websockets', :run_on_browserstack => true do
+	before(:all) do
+		@config = BeEF::Core::Configuration.instance
+		p "This is the /n #@config "
+		@config.set('beef.credentials.user', "beef")
+    @config.set('beef.credentials.passwd', "beef")
+    @config.set('beef.http.websocket.secure', true)
+    @config.set('beef.http.websocket.enable', true)
+		@username = @config.get('beef.credentials.user')
+		p "This is the /n #@username "
+		@password = @config.get('beef.credentials.passwd')
+		p "This is the /n #@password "	
+		# Load BeEF extensions and modules
+		# Always load Extensions, as previous changes to the config from other tests may affect
+		# whether or not this test passes.
+		print_info "Loading in BeEF::Extensions"
+		BeEF::Extensions.load
+		sleep 2
 
-		  # Check if modules already loaded. No need to reload.
-	  	if @config.get('beef.module').nil?
-		  	print_info "Loading in BeEF::Modules"
-	  		BeEF::Modules.load
-        sleep 2
-        p BeEF::Modules.load
+		# Check if modules already loaded. No need to reload.
+		if @config.get('beef.module').nil?
+			print_info "Loading in BeEF::Modules"
+			BeEF::Modules.load
+			sleep 2
+			p BeEF::Modules.load
+		else
+				print_info "Modules already loaded"
+		end
 
-	  	else
-	  			print_info "Modules already loaded"
-	  	end
+		# Grab DB file and regenerate if requested
+		print_info "Loading database"
+		db_file = @config.get('beef.database.file')
 
-		  # Grab DB file and regenerate if requested
-		  print_info "Loading database"
-	  	db_file = @config.get('beef.database.file')
+		if BeEF::Core::Console::CommandLine.parse[:resetdb]
+			print_info 'Resetting the database for BeEF.'
+			File.delete(db_file) if File.exists?(db_file)
+		end
 
-	  	if BeEF::Core::Console::CommandLine.parse[:resetdb]
-		  	print_info 'Resetting the database for BeEF.'
-		  	File.delete(db_file) if File.exists?(db_file)
-		  end
+		# Load up DB and migrate if necessary
+		ActiveRecord::Base.logger = nil
+		OTR::ActiveRecord.migrations_paths = [File.join('core', 'main', 'ar-migrations')]
+		OTR::ActiveRecord.configure_from_hash!(adapter:'sqlite3', database: db_file)
 
-	  	# Load up DB and migrate if necessary
-	  	ActiveRecord::Base.logger = nil
-	  	OTR::ActiveRecord.migrations_paths = [File.join('core', 'main', 'ar-migrations')]
-		  OTR::ActiveRecord.configure_from_hash!(adapter:'sqlite3', database: db_file)
+		context = ActiveRecord::Migration.new.migration_context
+		if context.needs_migration?
+		  ActiveRecord::Migrator.new(:up, context.migrations, context.schema_migration).migrate
+		end
 
-		  context = ActiveRecord::Migration.new.migration_context
-	  	if context.needs_migration?
-		    ActiveRecord::Migrator.new(:up, context.migrations, context.schema_migration).migrate
-	  	end
+		sleep 2
 
-	  	sleep 2
+		BeEF::Core::Migration.instance.update_db!
 
-		  BeEF::Core::Migration.instance.update_db!
+		# Spawn HTTP Server
+		print_info "Starting HTTP Hook Server"
+		http_hook_server = BeEF::Core::Server.instance
+		http_hook_server.prepare
 
-	  	# Spawn HTTP Server
-	  	print_info "Starting HTTP Hook Server"
-		  http_hook_server = BeEF::Core::Server.instance
-      http_hook_server.prepare
-    
-    	# Generate a token for the server to respond with
-      @token = BeEF::Core::Crypto::api_token
-    
-      @pids = fork do
-        BeEF::API::Registrar.instance.fire(BeEF::API::Server, 'pre_http_start', http_hook_server)
-      end
-      @pid = fork do
-        http_hook_server.start
-       end
-      # wait for server to start
-      sleep 2
+		# Generate a token for the server to respond with
+		@token = BeEF::Core::Crypto::api_token
 
-      #start the hook server instance, for it out to track the pids for graceful closure
-	  	@caps = CONFIG['common_caps'].merge(CONFIG['browser_caps'][TASK_ID])
-	  	@caps["name"] = self.class.description || ENV['name'] || 'no-name'
-      @caps["browserstack.local"] = true
-      @caps['browserstack.localIdentifier'] = ENV['BROWSERSTACK_LOCAL_IDENTIFIER']
+		# Initiate server start-up
+		@pids = fork do
+			BeEF::API::Registrar.instance.fire(BeEF::API::Server, 'pre_http_start', http_hook_server)
+		end
+		@pid = fork do
+			http_hook_server.start
+		end
 
-	  	@driver = Selenium::WebDriver.for(:remote,
-	  			:url => "http://#{CONFIG['user']}:#{CONFIG['key']}@#{CONFIG['server']}/wd/hub",
-		  		:desired_capabilities => @caps)
+		# Give the server time to start-up
+		sleep 1
 
-      # Hook new victim
-	  	print_info 'Hooking a new victim, waiting a few seconds...'
-      @driver.navigate.to "#{VICTIM_URL}"
-      p @driver
-	  	# Give time for browser hook to occur
-      sleep 2.5
-      #prepare for the HTTP model
-      @hooks = JSON.parse(RestClient.get "#{RESTAPI_HOOKS}?token=#{@token}")
-      p @hooks
-      @session = @hooks['hooked-browsers']
-      p @session
+		@caps = CONFIG['common_caps'].merge(CONFIG['browser_caps'][TASK_ID])
+		@caps["name"] = self.class.description || ENV['name'] || 'no-name'
+		@caps["browserstack.local"] = true
+		@caps['browserstack.localIdentifier'] = ENV['BROWSERSTACK_LOCAL_IDENTIFIER']
 
-  end
+		@driver = Selenium::WebDriver.for(:remote,
+				:url => "http://#{CONFIG['user']}:#{CONFIG['key']}@#{CONFIG['server']}/wd/hub",
+				:desired_capabilities => @caps)
 
-  after(:all) do
-    puts @driver 
-  #  @driver.quit
-    # cleanup: delete test browser entries and session
-    # kill the server
-    @config.set('beef.http.websocket.enable', false)
-    Process.kill("KILL", @pid)
-    Process.kill("KILL", @pids)
-    puts "waiting for server to die.."
-  end
 
-  it 'can hook a browser with websockets' do
-    #require 'byebug'; byebug
-    p "this is th https session that gets deleted"
-    expect(@session).not_to be_empty
-    #https.where(:hooked_browser_id => @session['0']['session']).delete_all
-  end
+		# Hook new victim
+		print_info 'Hooking a new victim, waiting a few seconds...'
+		@driver.navigate.to "#{VICTIM_URL}"
+		# Give time for browser hook to occur
+		sleep 2.5
+
+		@hooks = JSON.parse(RestClient.get "#{RESTAPI_HOOKS}?token=#{@token}")
+		p @hooks
+		@session = @hooks['hooked-browsers']['online']['0']['session']
+		p @session
+	end
+
+	after(:all) do
+		@driver.quit
+
+		print_info "Shutting down server"
+		Process.kill("KILL",@pid)
+		Process.kill("KILL",@pids)
+	end
+
+	it 'can successfully hook a browser' do
+    expect(@hooks['hooked-browsers']['online']).not_to be_empty
+	end
+
+	it 'browser details handler working' do
+		print_info "Getting browser details"
+		response = RestClient.get "#{RESTAPI_HOOKS}/#{@session}?token=#{@token}"
+    p response
+    details = JSON.parse(response.body)
+    p details
+		expect(@driver.browser.to_s.downcase).to eq(details['browser.name.friendly'].downcase).or eq('internet_explorer').or eq('msedge')
+	end
 end
