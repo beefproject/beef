9a4ad1ebb9512d0549667aa7c35ac8d45e80a6f1
Jack Walker <jwrc@protonmail.com>
2020-04-28 17:15:18 +1000

    Testing new client side wait to increase hook consistency in test.

--- spec/beef/core/main/autorun_engine/autorun_engine_spec.rb
+++ spec/beef/core/main/autorun_engine/autorun_engine_spec.rb
@@ -93,7 +93,8 @@ RSpec.describe 'AutoRunEngine Test', :run_on_browserstack => true do
 		@driver.navigate.to "#{VICTIM_URL}"
 
 		# Give time for browser hook to occur
-		sleep 3.5
+		wait = Selenium::WebDriver::Wait.new(:timeout => 30) # seconds
+		wait.until { @driver.execute_script("return !!window.beef.session").equal? true}
 
 		@hooks = JSON.parse(RestClient.get "#{RESTAPI_HOOKS}?token=#{@token}")
 		@session = @hooks['hooked-browsers']['online']['0']['session']
