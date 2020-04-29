bb2879afcdd1dca11b773af5ee698752626338fd
Jack Walker <jwrc@protonmail.com>
2020-04-29 16:04:16 +1000

    Added more handling to allow browser room to be hooked comfortably

--- spec/beef/modules/debug/test_beef_debugs_spec.rb
+++ spec/beef/modules/debug/test_beef_debugs_spec.rb
@@ -91,9 +91,13 @@ RSpec.describe 'BeEF Debug Command Modules:', :run_on_browserstack => true do
         print_info 'Hooking a new victim, waiting a few seconds...'
         @driver.navigate.to "#{VICTIM_URL}"
 
+		sleep 20
+
         # Give time for browser hook to occur
         wait = Selenium::WebDriver::Wait.new(:timeout => 30) # seconds
-		sleep 15 until wait.until { @driver.execute_script("return window.beef.session.get_hook_session_id().length") > 0}
+        wait.until { @driver.execute_script("return window.beef.session.get_hook_session_id().length") > 0}
+        
+		sleep 20
 
         @hooks = JSON.parse(RestClient.get "#{RESTAPI_HOOKS}?token=#{@token}")
         if @hooks['hooked-browsers']['online'].empty?
