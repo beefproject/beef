18e76512de20df99254a48315256be968f6970b5
Jack Walker <jwrc@protonmail.com>
2020-05-27 17:41:20 +1000

    Added some additional session handling code in the server setup.

--- spec/beef/modules/debug/test_beef_debugs_spec.rb
+++ spec/beef/modules/debug/test_beef_debugs_spec.rb
@@ -98,6 +98,12 @@ RSpec.describe 'BeEF Debug Command Modules:', :run_on_browserstack => true do
 
         @hook_request = RestClient.get "#{RESTAPI_HOOKS}?token=#{@token}"
         @hooks = JSON.parse(@hook_request)
+        
+        if @hooks['hooked-browsers']['online'].empty?
+          @session = @driver.execute_script("return window.beef.session.get_hook_session_id()")
+        else
+          @session = @hooks['hooked-browsers']['online'][0]
+        end
 
         # Grab Command Module IDs as they can differ from machine to machine
         @debug_mod_ids = JSON.parse(RestClient.get "#{RESTAPI_MODULES}?token=#{@token}")
