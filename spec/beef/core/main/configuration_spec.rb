b76a8d18656453739a589b6972006b3edefd4076
whealtey <wheatand@gmail.com>
2021-07-18 02:01:38 +0000

    added local host getter to configuration class

--- spec/beef/core/main/configuration_spec.rb
+++ spec/beef/core/main/configuration_spec.rb
@@ -1,5 +1,14 @@
 RSpec.describe 'BeEF Configuration' do
-  it 'should return the host value within the configuration file' do
-    
+  before(:all) do
+    @config_instance = BeEF::Core::Configuration.instance
+  end
+  it 'should set the local host value to 0.0.0.0' do
+    @config_instance.set('beef.http.host', '0.0.0.0')
+    expect(@config_instance.get('beef.http.host')).to eq('0.0.0.0')
+  end
+
+  it 'should get the local host value' do
+    @config_instance.set('beef.http.host', '0.0.0.0')
+    expect(@config_instance.local_host).to eq('0.0.0.0')
   end
 end
