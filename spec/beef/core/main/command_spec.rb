ad9f3a2f9529ba56b6fa1aba9cb8d96921ebe656
wheatley <wheatand@gmail.com>
2021-08-20 11:07:50 +1000

    add spec to check if configuration instance exists by setting and accessing a config variable

--- spec/beef/core/main/command_spec.rb
+++ spec/beef/core/main/command_spec.rb
@@ -1,5 +1,11 @@
 RSpec.describe 'BeEF Core Command Class specs' do
   before(:example) do
-    BEEF::Core::Modules.load
+    BeEF::Modules.load
+  end
+
+  it 'should have a configuration object accessable from class instance' do
+    command = BeEF::Core::Command.new('get_visited_urls')
+    command.config.set('beef.http.host', 'localhost')
+    expect(command.config.beef_host).to eq('localhost')
   end
 end
