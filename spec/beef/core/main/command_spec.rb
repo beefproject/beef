bbbabba6449e88ea7605a84bdb5df8dbc096521e
wheatley <wheatand@gmail.com>
2021-08-24 19:11:16 +1000

    removed some unessessary checks

--- spec/beef/core/main/command_spec.rb
+++ spec/beef/core/main/command_spec.rb
@@ -6,18 +6,5 @@ RSpec.describe 'BeEF Command class testing' do
     require 'modules/browser/hooked_domain/get_page_links/module'
     gpl = Get_page_links.new('test_get_variable')
     expect(gpl.config.beef_host).to eq('0.0.0.0')
-    @config_instance = BeEF::Core::Configuration.instance
-    @config_instance.set('beef.http.host', 'localhost')
-    @config_instance.set('beef.http.port', nil)
-    @config_instance.set('beef.https.enabled', true)
-    @config_instance.set('beef.https.public_enabled', false)
-    @config_instance.set('beef.http.public', nil)
-    @config_instance.set('beef.http.public_port', nil)
-    expect(@config_instance.get('beef.http.host')).to eq('localhost')
-    expect(@config_instance.get('beef.http.port')).to eq(nil)
-    expect(@config_instance.get('beef.https.enabled')).to eq(true)
-    expect(@config_instance.get('beef.https.public_enabled')).to eq(false)
-    expect(@config_instance.beef_url_str).to eq('https://localhost:3000')
-    
   end
 end
