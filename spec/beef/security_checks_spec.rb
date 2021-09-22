a8856a6f1df1f05ffdba3989de52d0b689b592ff
Isaac Powell <36595182+DeezyE@users.noreply.github.com>
2021-09-23 08:58:32 +1000

    remove redundant escape characters
    Co-authored-by: bcoles <bcoles@gmail.com>

--- spec/beef/security_checks_spec.rb
+++ spec/beef/security_checks_spec.rb
@@ -3,8 +3,8 @@ RSpec.describe 'BeEF Security Checks' do
     Dir['**/*.rb'].each do |path|
       File.open(path) do |f|
         next if /#{File.basename(__FILE__)}/.match(path) # skip this file
-        next if %r{\/msf-test\/}.match(path) # skip this file
-        next if %r{extensions\/dns}.match(path) # skip this file
+        next if %r{/msf-test/}.match(path) # skip this file
+        next if %r{extensions/dns}.match(path) # skip this file
 
         f.grep(/\Weval\W/im) do |line|
           raise "Illegal use of 'eval' found in\n Path: #{path}\nLine:  #{line}"
