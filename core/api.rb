19ff499a89026dbcc7cbe93a9f7bfc49c3f83e7c
Isaac Powell <36595182+DeezyE@users.noreply.github.com>
2021-10-15 10:36:54 +1000

    Rubocop api.rb fix (#2175)

--- core/api.rb
+++ core/api.rb
@@ -67,6 +67,7 @@ module BeEF
           next unless r['class'] == clss
           next unless r['method'] == method
           next unless is_matched_params? r, params
+
           return true
         end
         false
@@ -86,6 +87,7 @@ module BeEF
           next unless r['class'] == clss
           next unless r['method'] == method
           next unless is_matched_params? r, params
+
           return true
         end
         false
@@ -214,7 +216,7 @@ require 'core/api/modules'
 require 'core/api/extension'
 require 'core/api/extensions'
 require 'core/api/main/migration'
-require 'core/api/main/network_stack/assethandler.rb'
+require 'core/api/main/network_stack/assethandler'
 require 'core/api/main/server'
 require 'core/api/main/server/hook'
 require 'core/api/main/configuration'
