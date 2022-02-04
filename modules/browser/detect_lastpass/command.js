e155655b083be659d4d09f5cdda65bb0b342f086
gary brolsma <garethclyperis@gmail.com>
2022-02-03 19:49:03 -0500

    removing redundant code

--- modules/browser/detect_lastpass/command.js
+++ modules/browser/detect_lastpass/command.js
@@ -4,15 +4,11 @@
 // See the file 'doc/COPYING' for copying permission
 //
 
-$j = jQuery.noConflict();
-
 beef.execute(function() {
 	var result = "Not in use or not installed";
 
 	// The following base64 encoded string represents the LastPass inline PNG which is inserted into user/pass form fields
 	var base64PNG = "iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC";
-
-	while (result)
 
 	// Detect input form fields with the injected LastPass PNG as background image
 	var bginput =  $j('input[style]');
