fd8d6838e0bc31dadf94fe9fc9b78833565efcab
Artem <artem-belocerkov@yandex.ru>
2022-07-20 06:32:29 -0700

    Fixed bug when response_data is empty when http.response_data.length <= 100K

--- extensions/requester/rest/requester.rb
+++ extensions/requester/rest/requester.rb
@@ -229,9 +229,13 @@ module BeEF
         def response2hash(http)
           response_data = ''
 
-          if !http.response_data.nil? && (http.response_data.length > (1024 * 100)) # more thank 100K
-            response_data = http.response_data[0..(1024 * 100)]
-            response_data += "\n<---------- Response Data Truncated---------->"
+          if !http.response_data.nil?
+            if (http.response_data.length > (1024 * 100)) # more thank 100K
+              http.response_data = http.response_data[0..(1024 * 100)]
+              http.response_data += "\n<---------- Response Data Truncated---------->"
+            end
+
+            response_data = http.response_data
           end
 
           response_headers = ''
