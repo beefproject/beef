b308d8c643946bf8219f2c3c1d91e253b52ce3de
bcoles <bcoles@gmail.com>
2023-04-02 02:43:04 +1100

    AutorunEngine: RuleLoader: Skip enabled rule file if rule is already in database

--- core/main/autorun_engine/parser.rb
+++ core/main/autorun_engine/parser.rb
@@ -18,68 +18,63 @@ module BeEF
         VERSION = ['<', '<=', '==', '>=', '>', 'ALL', 'Vista', 'XP']
         CHAIN_MODE = %w[sequential nested-forward]
         MAX_VER_LEN = 15
-        # Parse a JSON ARE file and returns an Hash with the value mappings
-        def parse(name, author, browser, browser_version, os, os_version, modules, exec_order, exec_delay, chain_mode)
-          success = [true]
 
-          return [false, 'Illegal chain_mode definition'] unless CHAIN_MODE.include?(chain_mode)
-          return [false, 'Illegal rule name'] unless BeEF::Filters.is_non_empty_string?(name)
-          return [false, 'Illegal author name'] unless BeEF::Filters.is_non_empty_string?(author)
+        def parse(name, author, browser, browser_version, os, os_version, modules, execution_order, execution_delay, chain_mode)
+          raise ArgumentError, "Invalid rule name: #{name}" unless BeEF::Filters.is_non_empty_string?(name)
+          raise ArgumentError, "Invalid author name: #{author}" unless BeEF::Filters.is_non_empty_string?(author)
+          raise ArgumentError, "Invalid chain_mode definition: #{chain_mode}" unless CHAIN_MODE.include?(chain_mode)
+          raise ArgumentError, "Invalid os definition: #{os}" unless OS.include?(os)
+
+          unless modules.size == execution_delay.size
+            raise ArgumentError, "Number of execution_delay values (#{execution_delay.size}) must be consistent with number of modules (#{modules.size})"
+          end
+          execution_delay.each { |delay| raise TypeError, "Invalid execution_delay value: #{delay}. Values must be Integers." unless delay.is_a?(Integer) }
+
+          unless modules.size == execution_order.size
+            raise ArgumentError, "Number of execution_order values (#{execution_order.size}) must be consistent with number of modules (#{modules.size})"
+          end
+          execution_order.each { |order| raise TypeError, "Invalid execution_order value: #{order}. Values must be Integers." unless order.is_a?(Integer) }
 
           # if multiple browsers were specified, check each browser
           if browser.is_a?(Array)
             browser.each do |b|
-              return [false, 'Illegal browser definition'] unless BROWSER.include?(b)
+              raise ArgumentError, "Invalid browser definition: #{browser}" unless BROWSER.include?(b)
             end
           # else, if only one browser was specified, check browser and browser version
           else
-            return [false, 'Illegal browser definition'] unless BROWSER.include?(browser)
+            raise ArgumentError, "Invalid browser definition: #{browser}" unless BROWSER.include?(browser)
 
             if browser_version != 'ALL' && !(VERSION.include?(browser_version[0, 2].gsub(/\s+/, '')) &&
                   BeEF::Filters.is_valid_browserversion?(browser_version[2..-1].gsub(/\s+/, '')) && browser_version.length < MAX_VER_LEN)
-              return [false, 'Illegal browser_version definition']
+              raise ArgumentError, "Invalid browser_version definition: #{browser_version}"
             end
           end
 
           if os_version != 'ALL' && !(VERSION.include?(os_version[0, 2].gsub(/\s+/, '')) &&
                   BeEF::Filters.is_valid_osversion?(os_version[2..-1].gsub(/\s+/, '')) && os_version.length < MAX_VER_LEN)
-            return [false, 'Illegal os_version definition']
+            return ArgumentError, "Invalid os_version definition: #{os_version}"
           end
-
-          return [false, 'Illegal os definition'] unless OS.include?(os)
 
           # check if module names, conditions and options are ok
           modules.each do |cmd_mod|
             mod = BeEF::Core::Models::CommandModule.where(name: cmd_mod['name']).first
-            if mod.nil?
-              return [false, "The specified module name (#{cmd_mod['name']}) does not exist"]
-            else
-              modk = BeEF::Module.get_key_by_database_id(mod.id)
-              mod_options = BeEF::Module.get_options(modk)
 
-              opt_count = 0
-              mod_options.each do |opt|
-                if opt['name'] == cmd_mod['options'].keys[opt_count]
-                  opt_count += 1
-                else
-                  return [false, "The specified option (#{cmd_mod['options'].keys[opt_count]
-                                                        }) for module (#{cmd_mod['name']}) does not exist"]
-                end
+            raise "The specified module name (#{cmd_mod['name']}) does not exist" if mod.nil?
+
+            modk = BeEF::Module.get_key_by_database_id(mod.id)
+            mod_options = BeEF::Module.get_options(modk)
+
+            opt_count = 0
+            mod_options.each do |opt|
+              if opt['name'] != cmd_mod['options'].keys[opt_count]
+                raise ArgumentError, "The specified option (#{cmd_mod['options'].keys[opt_count]}) for module (#{cmd_mod['name']}) was not specified"
               end
+
+              opt_count += 1
             end
           end
 
-          exec_order.each { |order| return [false, 'execution_order values must be Integers'] unless order.is_a?(Integer) }
-          exec_delay.each { |delay| return [false, 'execution_delay values must be Integers'] unless delay.is_a?(Integer) }
-
-          return [false, 'execution_order and execution_delay values must be consistent with modules numbers'] unless
-              modules.size == exec_order.size && modules.size == exec_delay.size
-
-          success
-        rescue StandardError => e
-          print_error e.message.to_s
-          print_debug e.backtrace.join("\n").to_s
-          [false, 'Something went wrong.']
+          true
         end
       end
     end
