20c70043a3d45f432f75087a6079d156bf8a9bb3
bcoles <bcoles@gmail.com>
2023-04-03 16:44:11 +1000

    Extensions: Demos: Modify Handler to extend the Router class

--- extensions/demos/api.rb
+++ extensions/demos/api.rb
@@ -9,23 +9,13 @@ module BeEF
       module RegisterHttpHandlers
         BeEF::API::Registrar.instance.register(BeEF::Extension::Demos::RegisterHttpHandlers, BeEF::API::Server, 'mount_handler')
 
+        #
+        # Mounts the handlers for the demos pages
+        #
+        # @param beef_server [BeEF::Core::Server] HTTP server instance
+        #
         def self.mount_handler(beef_server)
-          # mount everything in html directory to /demos/
-          path = "#{File.dirname(__FILE__)}/html/"
-          files = Dir["#{path}**/*"]
-
-          beef_server.mount('/demos', Rack::File.new(path))
-
-          files.each do |f|
-            # don't follow symlinks
-            next if File.symlink?(f)
-
-            mount_path = "/demos/#{f.sub(path, '')}"
-            if File.extname(f) == '.html'
-              # use handler to mount HTML templates
-              beef_server.mount(mount_path, BeEF::Extension::Demos::Handler.new(f))
-            end
-          end
+          beef_server.mount('/demos', BeEF::Extension::Demos::Handler.new)
         end
       end
     end
--- extensions/demos/handler.rb
+++ extensions/demos/handler.rb
@@ -6,46 +6,8 @@
 module BeEF
   module Extension
     module Demos
-      class Handler
-        def initialize(file_path)
-          if File.exist?(file_path)
-            @file_path = file_path
-          else
-            print_error "[Demos] File does not exist: #{file_path}"
-          end
-        end
-
-        def call(env)
-          @body = ''
-          @request = Rack::Request.new(env)
-          @params = @request.query_string
-          @response = Rack::Response.new(body = [], 200, header = {})
-          config = BeEF::Core::Configuration.instance
-          eruby = Erubis::FastEruby.new(File.read(@file_path))
-          @body << eruby.evaluate(
-            'hook_uri' => config.get('beef.http.hook_file')
-          )
-
-          @response = Rack::Response.new(
-            body = [@body],
-            status = 200,
-            header = {
-              'Pragma' => 'no-cache',
-              'Cache-Control' => 'no-cache',
-              'Expires' => '0',
-              'Content-Type' => 'text/html'
-            }
-          )
-        end
-
-        # @note String representing the absolute path to the .html file
-        @file_path
-
-        # @note Object representing the HTTP request
-        @request
-
-        # @note Object representing the HTTP response
-        @response
+      class Handler < BeEF::Core::Router::Router
+        set :public_folder, File.expand_path(File.dirname(__FILE__)) + '/html/'
       end
     end
   end
