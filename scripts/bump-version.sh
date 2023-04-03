eebb9b57812287c784807e91becdb5050798ad83
bcoles <bcoles@gmail.com>
2023-04-03 19:26:47 +1000

    scripts: shellcheck bump-version

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-git checkout -b "release/$2"
+if [[ -z "${1}" || -z "${2}" ]]; then
+  echo "Error: missing arguments"
+  exit 1
+fi
+
+echo "Updating version ${1} to ${2}"
+
+git checkout -b "release/${2}"
 sed -i '' -e "s/$1/$2/g" VERSION
 sed -i '' -e "s/\"version\": \"$1\"/\"version\": \"$2\"/g" package.json
 sed -i '' -e "s/\"version\": \"$1\"/\"version\": \"$2\"/g" package-lock.json
 sed -i '' -e "s/\"version\": \"$1\"/\"version\": \"$2\"/g" config.yaml
 
-if [[ "$1" != *"-pre"* ]]; then
-	sed -i '' -e "s/v$1/v$2/g" .github/ISSUE_TEMPLATE.md
-fi
-
 git add VERSION package.json package-lock.json config.yaml
-git commit -m "Version bump $2"
-git push --set-upstream origin release/$2
+git commit -m "Version bump ${2}"
+git push --set-upstream origin "release/${2}"
 git push
