4badc73afdbff8010c6dce4dfe01a16f5ddf9959
bcoles <bcoles@gmail.com>
2023-04-03 16:04:13 +1000

    Extensions: Demos: Remove unused demo assets

--- modules/browser/play_sound/module.rb
+++ modules/browser/play_sound/module.rb
@@ -4,21 +4,12 @@
 # See the file 'doc/COPYING' for copying permission
 #
 class Play_sound < BeEF::Core::Command
-  # set and return all options for this module
   def self.options
-    @configuration = BeEF::Core::Configuration.instance
-    proto = @configuration.beef_proto
-    beef_host = @configuration.beef_host
-    beef_port = @configuration.beef_port
-    base_host = "#{proto}://#{beef_host}:#{beef_port}"
-
-    sound_file_url = "#{base_host}/demos/sound.wav"
-
     [{
       'name' => 'sound_file_uri',
       'description' => 'The web accessible URI for the wave sound file.',
       'ui_label' => 'Sound File Path',
-      'value' => sound_file_url,
+      'value' => '',
       'width' => '300px'
     }]
   end
