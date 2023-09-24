6ab70ea8885f41d71af23c41c603532e195bfdd3
wheatley <wheatand@gmail.com>
2023-09-24 20:45:25 +1000

    reverted to old file name

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,7 +35,7 @@ end
 
 TASK_ID = (ENV['TASK_ID'] || 0).to_i
 print_info ENV['CONFIG_FILE']
-CONFIG_FILE = ENV['CONFIG_FILE'] || 'windows/win10/win11_chrome_103.config.yml'
+CONFIG_FILE = ENV['CONFIG_FILE'] || 'windows/win10/win10_chrome_81.config.yml'
 CONFIG = YAML.safe_load(File.read("./spec/support/browserstack/#{CONFIG_FILE}"))
 CONFIG['user'] = ENV['BROWSERSTACK_USERNAME'] || ''
 CONFIG['key'] = ENV['BROWSERSTACK_ACCESS_KEY'] || ''
