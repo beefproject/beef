01fc227d9073ed508ddb534770e85cb8de4d9318
Stephen <stephenahq@gmail.com>
2024-01-04 05:55:39 +1000

    Removed blog from loanding banners

--- core/main/console/banners.rb
+++ core/main/console/banners.rb
@@ -32,7 +32,7 @@ module BeEF
             print_info "Browser Exploitation Framework (BeEF) #{version}"
             data = "Twit: @beefproject\n"
             data += "Site: https://beefproject.com\n"
-            data += "Blog: http://blog.beefproject.com\n"
+            # data += "Blog: http://blog.beefproject.com\n"
             data += "Wiki: https://github.com/beefproject/beef/wiki\n"
             print_more data
             print_info 'Project Creator: ' + 'Wade Alcorn'.red + ' (@WadeAlcorn)'
