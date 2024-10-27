b230e5a4c8b60ab46008dd86fc689a53a2ef5474
zinduolis <ilgakulnis@gmail.com>
2024-10-27 16:48:56 +1000

    Troubleshooting

--- spec/beef/modules/debug/test_beef_debugs_spec.rb
+++ spec/beef/modules/debug/test_beef_debugs_spec.rb
@@ -92,8 +92,8 @@ RSpec.describe 'BeEF Debug Command Modules:', run_on_browserstack: true do
       @debug_mod_names_ids = {}
       @debug_mods = @debug_mod_ids.to_a.select do |cmd_mod|
         puts "cmd_mod:"
-        p cmd_mod[1]
-        category = Array(cmd_mod[1]['category']) # Ensure category is always an array
+        p cmd_mod
+        category = Array(cmd_mod[1]&.dig('category')) # Ensure category is always an array
         category.include?('Debug')
       end.map do |debug_mod|
         @debug_mod_names_ids[debug_mod[1]['class']] = debug_mod[1]['id']
