85d8d781123791d36d79a21dad24adb1cc3b3a70
kaitoozawa <kaitozaw@gmail.com>
2025-12-19 16:47:19 +1000

    add help command

--- core/main/console/commandline.rb
+++ core/main/console/commandline.rb
@@ -3,6 +3,8 @@
 # Browser Exploitation Framework (BeEF) - https://beefproject.com
 # See the file 'doc/COPYING' for copying permission
 #
+require 'optparse'
+
 module BeEF
   module Core
     module Console
@@ -38,7 +40,7 @@ module BeEF
               @options[:verbose] = true
             end
 
-            opts.on('-a', '--ascii_art', 'Prints BeEF ascii art') do
+            opts.on('-a', '--ascii-art', 'Prints BeEF ascii art') do
               @options[:ascii_art] = true
             end
 
@@ -54,14 +56,22 @@ module BeEF
               @options[:ws_port] = ws_port
             end
 
-            opts.on('-ud', '--update_disabled', 'Skips update') do
+            opts.on('-d', '--update-disabled', 'Skips update') do
               @options[:update_disabled] = true
             end
 
-            opts.on('-ua', '--update_auto', 'Automatic update with no prompt') do
+            opts.on('-u', '--update-auto', 'Automatic update with no prompt') do
               @options[:update_auto] = true
             end
 
+            opts.on('-h', '--help', 'Show this help') do
+              puts opts
+              exit 0
+            end
+
+            # opts.on('-i', '--interactive', 'Starts with the Console Shell activated') do
+            #  @options[:interactive] = true
+            # end
           end
 
           optparse.parse!
