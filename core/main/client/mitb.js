95793433fa71e6201116bfbcb49c0adde9efa758
zinduolis <ilgakulnis@gmail.com>
2025-12-26 19:18:05 +1000

    Update copyright year to 2026

--- core/main/client/mitb.js
+++ core/main/client/mitb.js
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2006-2025 Wade Alcorn - wade@bindshell.net
+// Copyright (c) 2006-2026 Wade Alcorn - wade@bindshell.net
 // Browser Exploitation Framework (BeEF) - https://beefproject.com
 // See the file 'doc/COPYING' for copying permission
 //
