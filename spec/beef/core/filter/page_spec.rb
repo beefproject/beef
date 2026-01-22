f923285da211d76d82aa38e216e191b7d6503221
Jake Webster <jakepeterwebster@gmail.com>
2026-01-22 17:13:44 +1000

    TEST: remaining core/filter specs

---
+++ spec/beef/core/filter/page_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe BeEF::Filters do
+  describe '.is_valid_pagetitle?' do
+    it 'validates page titles' do
+      expect(BeEF::Filters.is_valid_pagetitle?('Test Page')).to be(true)
+      expect(BeEF::Filters.is_valid_pagetitle?('A' * 500)).to be(true)
+      expect(BeEF::Filters.is_valid_pagetitle?('A' * 501)).to be(false)
+      expect(BeEF::Filters.is_valid_pagetitle?("\x00")).to be(false)
+    end
+  end
+
+  describe '.is_valid_pagereferrer?' do
+    it 'validates page referrers' do
+      expect(BeEF::Filters.is_valid_pagereferrer?('http://example.com')).to be(true)
+      expect(BeEF::Filters.is_valid_pagereferrer?('A' * 350)).to be(true)
+      expect(BeEF::Filters.is_valid_pagereferrer?('A' * 351)).to be(false)
+    end
+  end
+end
